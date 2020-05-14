93624d39c2daa1c51ab10f80b07a4bc969f61d69
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-14 21:16:33 +0900

    add: eslint設定を追加
    prettierと関連付け

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,25 +1,37 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    es2017: true,
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "babel-eslint",
+    sourceType: "module",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: true,
+        trailingComma: "none",
+        semi: false,
+      },
+    ],
+    "no-undef": 0,
   },
+  globals: {},
   overrides: [
     {
       files: [
         "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
       ],
       env: {
-        jest: true
-      }
-    }
-  ]
+        jest: true,
+      },
+    },
+  ],
 };
