5f2129655725c3a77299f420a3e803c35718bd1d
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-15 00:23:59 +0900

    add: store/index.jsを修正

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,8 +4,9 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  modules: {},
   state: {},
+  getters: {},
   mutations: {},
-  actions: {},
-  modules: {}
+  actions: {}
 })
