3ca0c90ece53f6803156d002b4bd2ad251303e48
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-14 21:18:10 +0900

    add: .prettierrc.jsを追加
    Prettier設定を網羅的に追加

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  rangeStart: 0,
+  filepath: 'none',
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf'
+}
