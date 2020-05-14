2c5b8706334804f8fb5b92d03aed3c019c4a1935
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-15 01:57:08 +0900

    add: Terraform関連変数を設定

---
+++ terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_account_id" {}
+variable "aws_region" {}
+variable "aws_resource_prefix" {}
+
+variable "domain" {}
+
+variable "github_token" {}
+variable "github_secret_token" {}
+variable "github_name" {}
+variable "github_organization" {}
+variable "github_repo" {}
+
+variable "images" {
+  default = {
+    us-east-1      = "ami-1ecae776"
+    us-west-2      = "ami-e7527ed7"
+    us-west-1      = "ami-d114f295"
+    eu-west-1      = "ami-a10897d6"
+    eu-central-1   = "ami-a8221fb5"
+    ap-southeast-1 = "ami-68d8e93a"
+    ap-southeast-2 = "ami-fd9cecc7"
+    ap-northeast-1 = "ami-cbf90ecb"
+    sa-east-1      = "ami-b52890a8"
+  }
+}
