645df258255453fe8632cbf0196b7abba24b668d
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-14 21:40:43 +0900

    fix: vue-cli-service lint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,36 +2,33 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    es2017: true,
+    es2017: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint",
-    sourceType: "module",
+    parser: 'babel-eslint',
+    sourceType: 'module'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "prettier/prettier": [
-      "error",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'error',
       {
         singleQuote: true,
-        trailingComma: "none",
-        semi: false,
-      },
+        trailingComma: 'none',
+        semi: false
+      }
     ],
-    "no-undef": 0,
+    'no-undef': 0
   },
   globals: {},
   overrides: [
     {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
-        jest: true,
-      },
-    },
-  ],
-};
+        jest: true
+      }
+    }
+  ]
+}
