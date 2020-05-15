877598da6702e5b13ddd5a9de6bbba9e43857889
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-15 21:02:44 +0900

    add: ecr.tfを作成 ECRにリポジトリを生成

---
+++ terraform/ecr.tf
@@ -0,0 +1,53 @@
+resource "aws_ecr_repository" "example_frontend" {
+  name = "${var.aws_resource_prefix}-frontend"
+}
+
+resource "aws_ecr_lifecycle_policy" "example_frontend" {
+  repository = aws_ecr_repository.example_frontend.name
+  policy     = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Keep last 10 release tagged images",
+        "selection": {
+          "tagStatus": "tagged",
+          "tagPrefixList": ["release"],
+          "countType": "imageCountMoreThan",
+          "countNumber": 10
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+EOF
+}
+
+resource "aws_ecr_repository" "example_backend" {
+  name = "${var.aws_resource_prefix}-backend"
+}
+
+resource "aws_ecr_lifecycle_policy" "example_backend" {
+  repository = aws_ecr_repository.example_backend.name
+  policy     = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Keep last 10 release tagged images",
+        "selection": {
+          "tagStatus": "tagged",
+          "tagPrefixList": ["release"],
+          "countType": "imageCountMoreThan",
+          "countNumber": 10
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+EOF
+}
