877bb8f6af2656e1814179c5da8b11feee2f042c
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-17 04:22:10 +0900

    add: usersリクエストスペック show機能をテスト

---
+++ backend/spec/requests/v1/users_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::Users', type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
+  it '特定のuserを取得する' do
+    get("/api/v1/users/#{user.id}")
+    json = JSON.parse(response.body)
+    expect(response).to have_http_status(200)
+    expect(json['user']['id']).to eq(user.id)
+  end
+end
