2ae2f74390a63baceab8adee86994d723d09288e
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-17 04:04:26 +0900

    add: rails g controller api::v1::users show

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations'
       }
+      resources :users, only: [:show]
     end
   end
 end
