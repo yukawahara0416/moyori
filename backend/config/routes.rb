829c9864e6fc3d83d73b9d0a81fe3f91637672ef
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-17 07:05:22 +0900

    add: routes.rbにTwitter認証時のコールバックをルーティング

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,10 +3,12 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   namespace :api, format: 'json' do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-        registrations: 'api/v1/auth/registrations'
+        registrations: 'api/v1/auth/registrations',
+        omniauth_callbacks: 'api/v1/auth/omniauth_callbacks'
       }
       resources :users, only: [:show]
     end
