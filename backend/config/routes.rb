7d296d65519e3ed2438f8c5d60982ff3c32724df
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-17 05:48:50 +0900

    add: rails g active_admin:install

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   namespace :api, format: 'json' do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
