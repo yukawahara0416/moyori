17a85a2c49a13e80a86401a302308a75cd58892a
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-17 01:50:04 +0900

    add: Userモデルスペック Userモデルの有効性をテスト

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -5,4 +5,37 @@ RSpec.describe User, type: :model do
     user = FactoryBot.build(:user)
     expect(user).to be_valid
   end
+
+  it 'name,email,passwordがあれば有効' do
+    user = User.new(
+      name: 'example name',
+      email: 'example@example.com',
+      password: 'examplepassword'
+    )
+    expect(user).to be_valid
+  end
+
+  it 'emailがなければ無効' do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+    expect(user.errors.messages[:email]).to include "can't be blank"
+  end
+
+  it 'passwordがなければ無効' do
+    user = FactoryBot.build(:user, password: nil)
+    user.valid?
+    expect(user.errors.messages[:password]).to include "can't be blank"
+  end
+
+  it 'passwordが5文字以下だと無効' do
+    user = FactoryBot.build(:user, password: 'a' * 5)
+    user.valid?
+    expect(user.errors.messages[:password]).to include 'is too short (minimum is 6 characters)'
+  end
+
+  it 'passwordが129文字以上だと無効' do
+    user = FactoryBot.build(:user, password: 'a' * 129)
+    user.valid?
+    expect(user.errors.messages[:password]).to include 'is too long (maximum is 128 characters)'
+  end
 end
