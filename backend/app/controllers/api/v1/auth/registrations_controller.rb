f16573a19d39aa0d276db392c1e1c0a67cd23eb2
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-17 00:44:20 +0900

    add: 継承を変更し、ストロングパラメータを追加

--- backend/app/controllers/api/v1/auth/registrations_controller.rb
+++ backend/app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,7 +1,16 @@
 module Api
   module V1
     module Auth
-      class RegistrationsController < ApplicationController
+      class RegistrationsController < DeviseTokenAuth::RegistrationsController
+        private
+
+          def sign_up_params
+            params.permit(:name, :email, :password, :password_confirmation)
+          end
+
+          def account_update_params
+            params.permit(:name, :email)
+          end
       end
     end
   end
