2bdf78ea6d5bb311e1ec266ef492b2de3cf199f6
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-17 10:30:00 +0900

    add: Spotモデルスペック Spotファクトリの有効性をテスト

--- backend/spec/models/spot_spec.rb
+++ backend/spec/models/spot_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Spot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+
+  it 'spotファクトリが有効' do
+    spot = FactoryBot.create(:spot)
+    expect(spot).to be_valid
+  end
+
+  it 'user_id, place_id, title, lat, lngがあれば有効' do
+    lat = '33.583745'
+    lng = '130.325260'
+    spot = Spot.new(
+      user_id: user.id,
+      place_id: 'abcdefg12345',
+      title: 'example_spot',
+      lat: lat.to_f,
+      lng: lng.to_f
+    )
+    expect(spot).to be_valid
+  end
 end
