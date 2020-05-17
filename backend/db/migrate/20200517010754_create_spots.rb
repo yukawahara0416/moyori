221ba7f0ebb80d09ba4939a213d42bed9356a419
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-17 10:08:16 +0900

    add: rails g model Spot user:references

---
+++ backend/db/migrate/20200517010754_create_spots.rb
@@ -0,0 +1,9 @@
+class CreateSpots < ActiveRecord::Migration[5.2]
+  def change
+    create_table :spots do |t|
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
