9151b29fe130c102177a5eb3cc43fd9fcb0878dd
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-19 11:44:18 +0900

    add: wifi_withouts_controller.rbにdestroyアクションを追加

--- backend/app/controllers/api/v1/wifi_withouts_controller.rb
+++ backend/app/controllers/api/v1/wifi_withouts_controller.rb
@@ -1,11 +1,17 @@
 module Api
   module V1
     class WifiWithoutsController < ApiController
-      before_action :authenticate_api_v1_user!, only: [:create]
+      before_action :authenticate_api_v1_user!, only: %i[create destroy]
 
       def create
         wifi_without = current_api_v1_user.wifi_without.create!(wifi_without_params)
         render json: wifi_without
+      end
+
+      def destroy
+        wifi_without = current_api_v1_user.wifi_without.find(params[:id])
+        wifi_without.destroy!
+        render json: wifi_without.as_json(only: :id)
       end
 
       private
