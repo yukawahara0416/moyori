37d3cab3850e26c489982ca506e7b6650099b2e6
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-19 11:28:45 +0900

    add: Wifi_withoutモデルスペック Wifi-withoutモデルの有効性をテスト

--- backend/spec/models/wifi_without_spec.rb
+++ backend/spec/models/wifi_without_spec.rb
@@ -8,4 +8,24 @@ RSpec.describe WifiWithout, type: :model do
     wifi_without = FactoryBot.create(:wifi_without)
     expect(wifi_without).to be_valid
   end
+
+  it 'user_id、spot_idがあれば有効' do
+    wifi_without = WifiWithout.new(
+      user_id: user.id,
+      spot_id: spot.id
+    )
+    expect(wifi_without).to be_valid
+  end
+
+  it 'user_idがなければ無効' do
+    wifi_without = FactoryBot.build(:wifi_without, user_id: nil)
+    wifi_without.valid?
+    expect(wifi_without.errors.messages[:user]).to include 'must exist'
+  end
+
+  it 'spot_idがなければ無効' do
+    wifi_without = FactoryBot.build(:wifi_without, spot_id: nil)
+    wifi_without.valid?
+    expect(wifi_without.errors.messages[:spot]).to include 'must exist'
+  end
 end
