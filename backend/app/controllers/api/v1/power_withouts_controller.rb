b4a4abd2f81e7b982097b4702dc7f7b2cef1959b
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-19 13:05:39 +0900

    add: power_withouts_controller.rbにcreateアクションを追加

--- backend/app/controllers/api/v1/power_withouts_controller.rb
+++ backend/app/controllers/api/v1/power_withouts_controller.rb
@@ -1,11 +1,21 @@
 module Api
   module V1
     class PowerWithoutsController < ApiController
+      before_action :authenticate_api_v1_user!, only: [:create]
+
       def create
+        power_without = current_api_v1_user.power_withouts.create!(power_without_params)
+        render json: power_without
       end
 
       def destroy
       end
+
+      private
+
+        def power_without_params
+          params.fetch(:power_without, {}).permit(:spot_id)
+        end
     end
   end
 end
