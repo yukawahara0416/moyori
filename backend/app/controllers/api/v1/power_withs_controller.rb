3875465c090e2fe7bc8c93d8f528759edf49b516
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-19 12:31:53 +0900

    add: power_withs_controller.rbにcreateアクションを追加

--- backend/app/controllers/api/v1/power_withs_controller.rb
+++ backend/app/controllers/api/v1/power_withs_controller.rb
@@ -1,11 +1,18 @@
 module Api
   module V1
     class PowerWithsController < ApiController
+      before_action :authenticate_api_v1_user!, only: [:create]
+
       def create
+        power_with = current_api_v1_user.power_withs.create!(power_with_params)
+        render json: power_with
       end
 
-      def destroy
-      end
+      private
+
+        def power_with_params
+          params.fetch(:power_with, {}).permit(:spot_id)
+        end
     end
   end
 end
