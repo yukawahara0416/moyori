bf4f0d29305c0d7054c22097edf10886a6ce3e45
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-19 12:54:17 +0900

    add: Power_withoutモデルスペック ファクトリの有効性をテスト

--- backend/spec/models/power_without_spec.rb
+++ backend/spec/models/power_without_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PowerWithout, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:spot) { FactoryBot.create(:spot) }
+
+  it 'power_withoutファクトリが有効' do
+    power_without = FactoryBot.create(:power_without)
+    expect(power_without).to be_valid
+  end
 end
