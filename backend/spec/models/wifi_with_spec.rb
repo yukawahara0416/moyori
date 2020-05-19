323364ed165717e0edd1672dcfe9e105c05c18a0
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-19 11:07:35 +0900

    add: Wifi_withモデルスペック Wifi_withモデルの有効性をテスト

--- backend/spec/models/wifi_with_spec.rb
+++ backend/spec/models/wifi_with_spec.rb
@@ -8,4 +8,24 @@ RSpec.describe WifiWith, type: :model do
     wifi_with = FactoryBot.create(:wifi_with)
     expect(wifi_with).to be_valid
   end
+
+  it 'user_id、spot_idがあれば有効' do
+    wifi_with = WifiWith.new(
+      user_id: user.id,
+      spot_id: spot.id
+    )
+    expect(wifi_with).to be_valid
+  end
+
+  it 'user_idがなければ無効' do
+    wifi_with = FactoryBot.build(:wifi_with, user_id: nil)
+    wifi_with.valid?
+    expect(wifi_with.errors.messages[:user]).to include 'must exist'
+  end
+
+  it 'spot_idがなければ無効' do
+    wifi_with = FactoryBot.build(:wifi_with, spot_id: nil)
+    wifi_with.valid?
+    expect(wifi_with.errors.messages[:spot]).to include 'must exist'
+  end
 end
