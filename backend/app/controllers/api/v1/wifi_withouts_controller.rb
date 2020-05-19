a999893cbbfcec0866c10242872f02c85915f48b
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-19 11:39:28 +0900

    add: rails g controller api::v1::wifi_withouts

---
+++ backend/app/controllers/api/v1/wifi_withouts_controller.rb
@@ -0,0 +1,11 @@
+module Api
+  module V1
+    class WifiWithoutsController < ApplicationController
+      def create
+      end
+
+      def destroy
+      end
+    end
+  end
+end
---
+++ backend/spec/requests/api/v1/wifi_withouts_request_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::WifiWithouts', type: :request do
+end
