617cce3794ef95011aa3118561c10a10e80e4715
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-19 11:40:52 +0900

    add: wifi_withouts_controller.rbにcreateアクションを追加

--- backend/app/controllers/api/v1/wifi_withouts_controller.rb
+++ backend/app/controllers/api/v1/wifi_withouts_controller.rb
@@ -1,11 +1,18 @@
 module Api
   module V1
-    class WifiWithoutsController < ApplicationController
+    class WifiWithoutsController < ApiController
+      before_action :authenticate_api_user!, only: [:create]
+
       def create
+        wifi_without = current_api_v1_user.wifi_without.create!(wifi_without_params)
+        render json: wifi_without
       end
 
-      def destroy
-      end
+      private
+
+        def wifi_without_params
+          params.fetch(:wifi_without, {}).permit(:spot_id)
+        end
     end
   end
 end
