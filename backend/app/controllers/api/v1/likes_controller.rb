ffe5194ee9fd635f122712d9750ac65fe9402dd1
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-19 10:12:45 +0900

    add: likes_controller.rbにcreateアクションを追加

--- backend/app/controllers/api/v1/likes_controller.rb
+++ backend/app/controllers/api/v1/likes_controller.rb
@@ -1,8 +1,18 @@
 module Api
   module V1
     class LikesController < ApiController
+      before_action :authenticate_api_v1_user!, only: [:create]
+
       def create
+        like = current_api_v1_user.likes.create!(likes_params)
+        render json: like
       end
+
+      private
+
+        def likes_params
+          params.fetch(:like, {}).permit(:spot_id)
+        end
     end
   end
 end
