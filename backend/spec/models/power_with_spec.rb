2fd476c227da63974549009cc10b073612c13d31
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-19 12:23:09 +0900

    add: Power_withモデルスペック Power_withモデルの有効性をテスト

--- backend/spec/models/power_with_spec.rb
+++ backend/spec/models/power_with_spec.rb
@@ -8,4 +8,24 @@ RSpec.describe PowerWith, type: :model do
     power_with = FactoryBot.create(:power_with)
     expect(power_with).to be_valid
   end
+
+  it 'user_id、spot_idがあれば有効' do
+    power_with = PowerWith.new(
+      user_id: user.id,
+      spot_id: spot.id
+    )
+    expect(power_with).to be_valid
+  end
+
+  it 'user_idがなければ無効' do
+    power_with = FactoryBot.build(:power_with, user_id: nil)
+    power_with.valid?
+    expect(power_with.errors.messages[:user]).to include 'must exist'
+  end
+
+  it 'spot_idがなければ無効' do
+    power_with = FactoryBot.build(:power_with, spot_id: nil)
+    power_with.valid?
+    expect(power_with.errors.messages[:spot]).to include 'must exist'
+  end
 end
