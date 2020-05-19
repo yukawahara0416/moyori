9003ab728d6f2abf1f122d515d75cf2dc136bae8
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-19 11:16:47 +0900

    add: wifi_with_controller.rbにcreateアクションを定義
    Postmanで動作を確認済み

--- backend/app/controllers/api/v1/wifi_withs_controller.rb
+++ backend/app/controllers/api/v1/wifi_withs_controller.rb
@@ -1,8 +1,18 @@
 module Api
   module V1
     class WifiWithsController < ApiController
+      before_action :authenticate_api_v1_user!, only: [:create]
+
       def create
+        wifi_with = current_api_v1_user.wifi_with.create!(wifi_with_params)
+        render json: wifi_with
       end
+
+      private
+
+        def wifi_with_params
+          params.fetch(:wifi_with, {}).permit(:spot_id)
+        end
     end
   end
 end
