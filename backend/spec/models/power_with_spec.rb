e307c06e2581a91934d0997d56ba21a3a4f600ed
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-19 12:22:38 +0900

    add: Power_withモデルスペック Power_withファクトリの有効性をテスト

--- backend/spec/models/power_with_spec.rb
+++ backend/spec/models/power_with_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PowerWith, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:spot) { FactoryBot.create(:spot) }
+
+  it 'power_withファクトリが有効' do
+    power_with = FactoryBot.create(:power_with)
+    expect(power_with).to be_valid
+  end
 end
