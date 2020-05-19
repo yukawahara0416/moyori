07249495471e9c196115277986e5621b7ee2d4a8
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-19 13:23:05 +0900

    add: Commentモデルスペック モデルの有効性をテスト

--- backend/app/models/comment.rb
+++ backend/app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :spot
+  validates :content, presence: true, length: { maximum: 1000 }
 end
--- backend/spec/models/comment_spec.rb
+++ backend/spec/models/comment_spec.rb
@@ -8,4 +8,37 @@ RSpec.describe Comment, type: :model do
     comment = FactoryBot.create(:comment)
     expect(comment).to be_valid
   end
+
+  it 'content、user_id、spot_idがあれば有効' do
+    comment = Comment.new(
+      content: 'example content',
+      user_id: user.id,
+      spot_id: spot.id
+    )
+    expect(comment).to be_valid
+  end
+
+  it 'contentがなければ無効' do
+    comment = FactoryBot.build(:comment, content: nil)
+    comment.valid?
+    expect(comment.errors.messages[:content]).to include "can't be blank"
+  end
+
+  it 'user_idがなければ無効' do
+    comment = FactoryBot.build(:comment, user_id: nil)
+    comment.valid?
+    expect(comment.errors.messages[:user]).to include 'must exist'
+  end
+
+  it 'spot_idがなければ無効' do
+    comment = FactoryBot.build(:comment, spot_id: nil)
+    comment.valid?
+    expect(comment.errors.messages[:spot]).to include 'must exist'
+  end
+
+  it 'commentが1001文字以上で無効' do
+    comment = FactoryBot.build(:comment, content: 'a' * 1001)
+    comment.valid?
+    expect(comment.errors.messages[:content]).to include 'is too long (maximum is 1000 characters)'
+  end
 end
