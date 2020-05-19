7ee3dca2f86f792bf0c9060455daf5e381d4a3da
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-19 13:22:28 +0900

    add: Commentモデルスペック ファクトリの有効性をテスト

--- backend/spec/models/comment_spec.rb
+++ backend/spec/models/comment_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:spot) { FactoryBot.create(:spot) }
+
+  it 'commentファクトリが有効' do
+    comment = FactoryBot.create(:comment)
+    expect(comment).to be_valid
+  end
 end
