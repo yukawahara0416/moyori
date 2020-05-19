2bf376246f528992591a0651330170e25daab1bb
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-19 12:54:37 +0900

    add: Power_withoutモデルスペック モデルの有効性をテスト

--- backend/spec/models/power_without_spec.rb
+++ backend/spec/models/power_without_spec.rb
@@ -8,4 +8,24 @@ RSpec.describe PowerWithout, type: :model do
     power_without = FactoryBot.create(:power_without)
     expect(power_without).to be_valid
   end
+
+  it 'user_id、spot_idがあれば有効' do
+    power_without = PowerWithout.new(
+      user_id: user.id,
+      spot_id: spot.id
+    )
+    expect(power_without).to be_valid
+  end
+
+  it 'user_idがなければ無効' do
+    power_without = FactoryBot.build(:power_without, user_id: nil)
+    power_without.valid?
+    expect(power_without.errors.messages[:user]).to include 'must exist'
+  end
+
+  it 'spot_idがなければ無効' do
+    power_without = FactoryBot.build(:power_without, spot_id: nil)
+    power_without.valid?
+    expect(power_without.errors.messages[:spot]).to include 'must exist'
+  end
 end
