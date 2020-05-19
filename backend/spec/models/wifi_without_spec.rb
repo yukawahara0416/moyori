d957f677df15815bb35017cf07d95919b5034759
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-19 11:28:10 +0900

    add: Wifi_withoutモデルスペック Wifi_withoutファクトリの有効性をテスト

--- backend/spec/models/wifi_without_spec.rb
+++ backend/spec/models/wifi_without_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe WifiWithout, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:spot) { FactoryBot.create(:spot) }
+
+  it 'wifi_withoutファクトリが有効' do
+    wifi_without = FactoryBot.create(:wifi_without)
+    expect(wifi_without).to be_valid
+  end
 end
