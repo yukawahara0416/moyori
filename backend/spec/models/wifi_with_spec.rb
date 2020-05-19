10f3f0c6e48a915e133ec1f5bc7276b17510e57c
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-19 11:06:50 +0900

    add: Wifi_withモデルスペック Wifi_withファクトリの有効性をテスト

--- backend/spec/models/wifi_with_spec.rb
+++ backend/spec/models/wifi_with_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe WifiWith, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:spot) { FactoryBot.create(:spot) }
+
+  it 'wifi_withファクトリが有効' do
+    wifi_with = FactoryBot.create(:wifi_with)
+    expect(wifi_with).to be_valid
+  end
 end
