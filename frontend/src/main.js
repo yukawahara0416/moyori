139d54bd4169989fbece6f46108f8e3ab0eb30a2
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-21 10:39:13 +0900

    add: プラグインをimport

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import router from '@/router'
 import store from '@/store'
 import vuetify from '@/plugins/vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import '@/plugins/maps.js'
 
 Vue.config.productionTip = process.env.NODE_ENV === 'production' ? false : true
 
