540e022f782ad66d89e23d554170cce79c0a30ff
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-21 09:26:37 +0900

    improve: productionTipの有無をNODE_ENVでスイッチさせる

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+import vuetify from '@/plugins/vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = process.env.NODE_ENV === 'production' ? false : true
 
 new Vue({
   el: '#app',
