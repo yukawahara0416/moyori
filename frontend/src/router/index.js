40c3b82cac0b2c271524e97fb8d495c91539bcd2
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-21 09:43:06 +0900

    add: Search.vueをルートへルーティング
    ルーティングはいつでも変更できるので開発中最もアクセスの多いSearch.vueをルートに設定した

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
+import Search from '@/views/Search.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
     component: Home
   },
@@ -17,6 +18,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/',
+    name: 'Search',
+    component: Search
   }
 ]
 
