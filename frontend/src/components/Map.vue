086e3b323e0f9781421fa947c2ebec1edc00ca67
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-21 17:11:06 +0900

    add: 周辺情報を取得する機能を追加

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -16,7 +16,8 @@
         :title="m.title"
       />
     </GmapMap>
-    <v-btn @click="moveToCurrentLocation()">現在地へ移動</v-btn>
+    <v-btn @click="moveToCurrentLocation">現在地へ移動</v-btn>
+    <v-btn @click="setNearbyMarkers">周辺情報を取得</v-btn>
     <p>lat: {{ currentCenter.lat }}</p>
     <p>lng: {{ currentCenter.lng }}</p>
   </div>
@@ -38,6 +39,20 @@ export default {
   },
 
   methods: {
+    // 現在地へ移動する
+    moveToCurrentLocation: async function() {
+      const pos = await this.getCurrentLocation()
+      await this.setCurrentLocationMarker(pos)
+      await this.panToLocation(pos)
+    },
+
+    // 周辺情報をマーカーで設置する
+    setNearbyMarkers: async function() {
+      const pos = this.currentCenter
+      await this.nearbySearch(pos)
+      // マーカーを設置する処理はココ
+    },
+
     // 現在地を取得する
     getCurrentLocation() {
       return new Promise(resolve => {
@@ -61,11 +76,28 @@ export default {
       })
     },
 
-    // 現在地へ移動する
-    moveToCurrentLocation: async function() {
-      const pos = await this.getCurrentLocation()
-      await this.setCurrentLocationMarker(pos)
-      await this.panToLocation(pos)
+    // 周辺の情報を取得する
+    nearbySearch(pos) {
+      return new Promise(resolve => {
+        const request = {
+          location: new google.maps.LatLng(pos.lat, pos.lng),
+          radius: 500,
+          type: ['cafe']
+        }
+        const map = this.$refs.map.$mapObject
+        const placeService = new google.maps.places.PlacesService(map)
+        const markers = placeService.nearbySearch(
+          request,
+          (results, status) => {
+            if (status == 'OK' || status == 'ZERO_RESULTS') {
+              console.log(results)
+            } else {
+              console.log('周辺の情報を取得中にエラーが発生しました')
+            }
+          }
+        )
+        resolve(markers)
+      })
     },
 
     // マップ中心を監視する
