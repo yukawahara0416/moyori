37bcf63dde605a28efa8cbb4c45acaf81a75d1aa
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-21 09:13:35 +0900

    improve: googlefontを追加

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,3 +7,12 @@
 <script>
 export default {}
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans+JP:100,300,400,500,700,900&display=swap&subset=japanese');
+
+#inspire {
+  background: #cccccc;
+  font-family: 'Noto Sans JP', sans-serif;
+}
+</style>
