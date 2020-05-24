9594f756a7545e1850705800fcd1e1e1cbb94567
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-25 06:14:36 +0900

    fix: config.stubを削除
    個別のspecファイルで対応する

--- frontend/tests/jest-setup.js
+++ frontend/tests/jest-setup.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import { config } from '@vue/test-utils'
 
 Vue.use(Vuetify)
 Vue.config.productionTip = process.env.NODE_ENV === 'production' ? false : true
 // Vue.config.silent = true
-
-config.stubs['GmapMap'] = '<div />'
-config.stubs['.v-btn'] = '<button />'
