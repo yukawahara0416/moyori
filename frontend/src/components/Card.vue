6ebb216fcb6815e660480fe73eba441a9e2bb38e
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-25 18:29:31 +0900

    add: Cardコンポーネントのテンプレ骨格を作成

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
     <h1>this is Card</h1>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-row>
+            <v-col>
+              <v-img></v-img>
+            </v-col>
+            <v-col>
+              <v-card-text></v-card-text>
+              <v-card-action></v-card-action>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
+
+<script>
+export default {}
+</script>
