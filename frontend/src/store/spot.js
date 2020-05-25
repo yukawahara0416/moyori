40eb250a43c3e0f705c115ca14422a5da3980c1e
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-26 07:27:26 +0900

    add: Map.vueからmarkersへsetできることを確認

--- frontend/src/store/spot.js
+++ frontend/src/store/spot.js
@@ -1,12 +1,23 @@
 export default {
   state: {
-    markers: [{ name: 'hoge', position: { lat: 33.574, lng: 130.331 } }]
+    markers: []
   },
-  actions: {},
   getters: {
     markers(state) {
       return state.markers
     }
   },
-  mutations: {}
+  mutations: {
+    pushToMarkers(state, payload) {
+      state.markers = state.markers.concat(payload)
+    }
+  },
+  actions: {
+    pushToMarkers(context, results) {
+      return new Promise(resolve => {
+        context.commit('pushToMarkers', results)
+        resolve()
+      })
+    }
+  }
 }
