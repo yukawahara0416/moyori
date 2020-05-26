786eda8aef84f0d70d4b2937e9bce8f6ecded080
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-26 15:52:10 +0900

    add: 選択中のマーカーのアイコンを変更する機能を実装

--- frontend/src/basics/Gmap/Marker.vue
+++ frontend/src/basics/Gmap/Marker.vue
@@ -6,6 +6,7 @@
       :icon="m.icon"
       :position="m.position"
       :title="m.name"
+      @click="switchMarker(m, id)"
     />
   </div>
 </template>
@@ -17,6 +18,22 @@ export default {
       type: Array,
       default: () => [],
       required: false
+    }
+  },
+
+  methods: {
+    switchMarker(marker, id) {
+      this.changeMarkerIcon(marker, id)
+    },
+    resetMarkerIcon() {},
+    cacheMarkerIcon() {},
+    changeMarkerIcon(marker, id) {
+      const selectingMarker = this.markers[id]
+      selectingMarker.icon = {
+        url: require('@/assets/spotlight.png'),
+        scaledSize: new google.maps.Size(50, 50)
+      }
+      selectingMarker.zIndex = 1000
     }
   }
 }
