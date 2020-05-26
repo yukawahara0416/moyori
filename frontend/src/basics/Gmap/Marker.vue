8fe7ba76eedce68ac8f578cf153ba363380440a1
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-05-26 16:16:53 +0900

    add: 直前に選択していたマーカーのアイコンを戻す機能を実装

--- frontend/src/basics/Gmap/Marker.vue
+++ frontend/src/basics/Gmap/Marker.vue
@@ -35,6 +35,18 @@ export default {
       this.changeMarkerIcon(marker, id)
     },
 
+    // 直前に選択していたマーカーのアイコンを戻す
+    resetMarkerIcon(marker, id) {
+      const targetMarker = this.markers[this.cacheMarker.id]
+      if (this.cacheMarker.id >= 0 && this.cacheMarker.id != id) {
+        targetMarker.icon = {
+          url: this.cacheMarker.icon,
+          scaledSize: new google.maps.Size(50, 50)
+        }
+        targetMarker.zIndex = 1
+      }
+    },
+
     // 選択したマーカーのアイコンを記録する（戻すときに必要）
     cacheMarkerIcon(marker, id) {
       if (this.cacheMarker.id != id) {
