4db3793fa4b54b202aa4bfae34f3f38227ec0df0
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-04 07:12:34 +0900

    add: markerStoreの単体テストを新規作成

---
+++ frontend/tests/unit/store/marker.spec.js
@@ -0,0 +1,55 @@
+import { createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import markerStore from '@/store/marker.js'
+import { cloneDeep } from 'lodash'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+let store
+
+beforeEach(() => {
+  store = new Vuex.Store(cloneDeep(markerStore))
+})
+
+describe('mutations', () => {
+  it('setMarkers', () => {
+    const setData = {
+      markers: [
+        { name: 'hoge', icon: 'foo' },
+        { name: 'fuga', icon: 'bar' }
+      ]
+    }
+    store.commit('setMarkers', setData.markers)
+    expect(store.state.markers).toStrictEqual(setData.markers)
+  })
+
+  it('clearMarkers', () => {
+    const setData = {
+      markers: [
+        { name: 'hoge', icon: 'foo' },
+        { name: 'fuga', icon: 'bar' }
+      ]
+    }
+    store.commit('setMarkers', setData.markers)
+    store.commit('clearMarkers')
+    expect(store.state.markers).toStrictEqual([])
+  })
+
+  it('setCurrentMarker', () => {
+    const setData = {
+      currentMarker: { id: 11, icon: 'hoge' }
+    }
+    store.commit('setCurrentMarker', setData.currentMarker)
+    expect(store.state.currentMarker).toStrictEqual(setData.currentMarker)
+  })
+
+  it('clearCurrentMarker', () => {
+    const setData = {
+      currentMarker: { id: 11, icon: 'hoge' }
+    }
+    store.commit('setCurrentMarker', setData.currentMarker)
+    store.commit('clearCurrentMarker')
+    expect(store.state.currentMarker).toStrictEqual({ icon: '', id: -1 })
+  })
+})
