f21bbeac814c1e5ed12a605283aa708cac17cdeb
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-04 18:05:21 +0900

    add: signupテンプレート作成

---
+++ frontend/src/components/SignUp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>SignUp</h2>
+    <input type="text" v-model="signUpFormData.name" placeholder="name" />
+    <input type="text" v-model="signUpFormData.email" placeholder="email" />
+    <input type="text" v-model="signUpFormData.password" placeholder="password" />
+    <input
+      type="text"
+      v-model="signUpFormData.password_confirmation"
+      placeholder="password_confirmation"
+    />
+    <button type="submit" v-on:click="signUp">アカウント登録</button>
+    <div>{{ signUpFormData }}</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
