925cb3a5345833dc2d2320a7fcd2f97165cad779
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-04 10:46:41 +0900

    add: actionsを追加

--- frontend/src/store/modules/user.js
+++ frontend/src/store/modules/user.js
@@ -15,17 +15,21 @@ export default {
       password_confirmation: ''
     }
   },
+
   getters: {
     currentUser(state) {
       return state.currentUser
     },
+
     signInFormData(state) {
       return state.signInFormData
     },
+
     signUpFormData(state) {
       return state.signUpFormData
     }
   },
+
   mutations: {
     currentUser(state, payload) {
       state.currentUser = payload.user
@@ -43,5 +47,28 @@ export default {
       state.currentUser = null
     }
   },
-  actions: {}
+  actions: {
+    signUp(content, signUpFormData) {
+      axios
+        .post('/api/v1/auth', signUpFormData)
+        .then(function(response) {
+          content.commit('currentUser', { user: response.data })
+          content.commit('signIn', response.headers)
+        })
+        .catch(function(error) {
+          context.commit('currentUser', { user: error })
+        })
+    },
+
+    signOut(context) {
+      axios
+        .delete('/api/v1/auth/sign_out', { headers: context.state.headers })
+        .then(function() {
+          context.commit('signOut')
+        })
+        .catch(function(error) {
+          alert('予期しないエラーが発生しました。')
+        })
+    }
+  }
 }
