c4ef46016884a7a3aee824dca38e072a81586645
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-04 18:05:09 +0900

    add: signinテンプレート作成

---
+++ frontend/src/components/SignIn.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>SignIn</h2>
+    <input type="text" v-model="signInFormData.email" placeholder="email" />
+    <input type="text" v-model="signInFormData.password" placeholder="password" />
+    <button type="submit" v-on:click="signIn">ログイン</button>
+    <div>{{ signInFormData }}</div>
+    <button v-show="currentUser" type="submit" v-on:click="signOut">ログアウト</button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
