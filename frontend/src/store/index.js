f5a6ea30b96398007c0c6d224824a919cfc31ef0
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-04 10:26:12 +0900

    add: module/userStoreを作成

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import markerStore from '@/store/marker.js'
+import userStore from '@/store/user.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    markerStore
+    markerStore,
+    userStore
   },
   state: {},
   getters: {},
