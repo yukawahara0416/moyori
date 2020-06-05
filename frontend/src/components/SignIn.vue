cab5d7c367f56795716b0cffd06c2b126a42ea64
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-05 12:19:01 +0900

    improve: 整形

--- frontend/src/components/SignIn.vue
+++ frontend/src/components/SignIn.vue
@@ -2,10 +2,16 @@
   <div>
     <h2>SignIn</h2>
     <input type="text" v-model="signInFormData.email" placeholder="email" />
-    <input type="text" v-model="signInFormData.password" placeholder="password" />
+    <input
+      type="text"
+      v-model="signInFormData.password"
+      placeholder="password"
+    />
     <button type="submit" v-on:click="signIn">ログイン</button>
     <div>{{ signInFormData }}</div>
-    <button v-show="currentUser" type="submit" v-on:click="signOut">ログアウト</button>
+    <button v-show="currentUser" type="submit" v-on:click="signOut">
+      ログアウト
+    </button>
   </div>
 </template>
 
