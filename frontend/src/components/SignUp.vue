f7e27273925db86751a9a16fc0ab4e7912fe3683
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-05 12:21:31 +0900

    add: getters・actionsを追加

--- frontend/src/components/SignUp.vue
+++ frontend/src/components/SignUp.vue
@@ -3,17 +3,33 @@
     <h2>SignUp</h2>
     <input type="text" v-model="signUpFormData.name" placeholder="name" />
     <input type="text" v-model="signUpFormData.email" placeholder="email" />
-    <input type="text" v-model="signUpFormData.password" placeholder="password" />
+    <input
+      type="text"
+      v-model="signUpFormData.password"
+      placeholder="password"
+    />
     <input
       type="text"
       v-model="signUpFormData.password_confirmation"
       placeholder="password_confirmation"
     />
-    <button type="submit" v-on:click="signUp">アカウント登録</button>
+    <button type="submit" @click="signUp">アカウント登録</button>
     <div>{{ signUpFormData }}</div>
   </div>
 </template>
 
 <script>
-export default {}
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters(['signUpFormData', 'currentUser'])
+  },
+
+  methods: {
+    signUp() {
+      this.$store.dispatch('signUp', this.signUpFormData)
+    }
+  }
+}
 </script>
