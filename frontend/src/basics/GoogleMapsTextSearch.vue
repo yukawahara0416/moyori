44c83467fe04e9a00b4d8b0b7ddc52d0906e9be0
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-06 10:36:48 +0900

    improve: textsearchコンポーネントを分離

---
+++ frontend/src/basics/GoogleMapsTextSearch.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-text-field
+      data-test="textsearch"
+      label="キーワード検索"
+      type="text"
+      v-model="keyword"
+      @keydown.enter="textSearch"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      keyword: ''
+    }
+  },
+
+  methods: {
+    textSearch() {
+      this.$emit('text-search', this.keyword)
+    }
+  }
+}
+</script>
