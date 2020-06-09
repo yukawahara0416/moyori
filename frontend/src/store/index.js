2eb0a034a3ade7d09edfeb9bdfa20b98037ea24d
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-09 15:52:23 +0900

    add: rootStoreにlikeをモジュール定義

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import markerStore from '@/store/modules/marker.js'
 import userStore from '@/store/modules/user.js'
+import likeStore from '@/store/modules/spot/like.js'
 import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
@@ -9,7 +10,8 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   modules: {
     markerStore,
-    userStore
+    userStore,
+    likeStore
   },
 
   plugins: [
