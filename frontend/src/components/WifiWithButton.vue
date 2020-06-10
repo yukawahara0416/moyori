88b1426f54fbd33d2e78edb4475ba15826447209
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-10 14:21:02 +0900

    add: propsを追加

---
+++ frontend/src/components/WifiWithButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-btn icon @click="wifiWithHandler()">
+      wifi
+    </v-btn>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  props: {
+    spot: Object,
+    id: Number
+  }
+}
+</script>
+
+<style></style>
