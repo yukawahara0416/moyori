9b6fd49fe25b978c1f9c0c00c89bdd4e0f895edf
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-10 17:04:31 +0900

    add: method/unWifiWithを追加
    すでにWifiあるよが投票されていたらその投票を取り消す必要があるため

--- frontend/src/components/WifiWithoutButton.vue
+++ frontend/src/components/WifiWithoutButton.vue
@@ -86,6 +86,11 @@ export default {
     unWifiWithout(wifiWithout, id) {
       var params = { id: wifiWithout.id }
       this.$store.dispatch('unWifiWithout', { params: params, id: id })
+    },
+
+    unWifiWith(wifiWith, id) {
+      var params = { id: wifiWith.id }
+      this.$store.dispatch('unWifiWith', { params: params, id: id })
     }
   }
 }
