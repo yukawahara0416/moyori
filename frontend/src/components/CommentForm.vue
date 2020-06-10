11a2ca55a35b213d61736bf26b3b517ce95047c9
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-11 07:19:59 +0900

    add: テンプレートを追加

--- frontend/src/components/CommentForm.vue
+++ frontend/src/components/CommentForm.vue
@@ -1,4 +1,19 @@
-<template> </template>
+<template>
+  <div>
+    <v-form @submit.prevent>
+      <v-text-field
+        label="コメント"
+        name="comment"
+        type="text"
+        v-model="content"
+      />
+    </v-form>
+
+    <v-btn @click="commentHandler()" type="submit">
+      コメント
+    </v-btn>
+  </div>
+</template>
 
 <script>
 export default {}
