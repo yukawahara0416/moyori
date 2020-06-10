c2dae462c56578b412447d7df842dd57ce98a448
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-10 17:49:28 +0900

    add: action/powerWith, unPowerWithを追加

--- frontend/src/store/modules/spot/powerWith.js
+++ frontend/src/store/modules/spot/powerWith.js
@@ -9,4 +9,29 @@ const axiosBase = axios.create({
   responseType: 'json'
 })
 
-export default {}
+export default {
+  actions: {
+    powerWith(context, { params, id }) {
+      axiosBase
+        .post('/api/v1/power_withs', params, {
+          headers: context.rootState.userStore.headers
+        })
+        .then(function(response) {
+          context.commit('addPowerWith', { powerWith: response.data, id: id })
+        })
+    },
+
+    unPowerWith(context, { params, id }) {
+      axiosBase
+        .delete('/api/v1/power_withs/' + params.id, {
+          headers: context.rootState.userStore.headers
+        })
+        .then(function(response) {
+          context.commit('deletePowerWith', {
+            powerWith: response.data,
+            id: id
+          })
+        })
+    }
+  }
+}
