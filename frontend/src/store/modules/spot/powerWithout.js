c2c3958781efa0adf7ae375c11a0c00f9fba465d
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-11 05:44:51 +0900

    add: axios基本設定を定義

---
+++ frontend/src/store/modules/spot/powerWithout.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const axiosBase = axios.create({
+  baseURL: 'http://localhost:3000',
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest'
+  },
+  responseType: 'json'
+})
+
+export default {}
