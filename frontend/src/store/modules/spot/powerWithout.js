1218188bcddda44e562d917745e99f9351122298
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-11 05:45:56 +0900

    add: action/powerWithout, unPowerWithoutを追加

--- frontend/src/store/modules/spot/powerWithout.js
+++ frontend/src/store/modules/spot/powerWithout.js
@@ -9,4 +9,32 @@ const axiosBase = axios.create({
   responseType: 'json'
 })
 
-export default {}
+export default {
+  actions: {
+    powerWithout(context, { params, id }) {
+      axiosBase
+        .post('/api/v1/power_withouts', params, {
+          headers: context.rootState.userStore.headers
+        })
+        .then(function(response) {
+          context.commit('addPowerWithout', {
+            powerWithout: response.data,
+            id: id
+          })
+        })
+    },
+
+    unPowerWithout(context, { params, id }) {
+      axiosBase
+        .delete('/api/v1/power_withouts/' + params.id, {
+          headers: context.rootState.userStore.headers
+        })
+        .then(function(response) {
+          context.commit('deletePowerWithout', {
+            powerWithout: response.data,
+            id: id
+          })
+        })
+    }
+  }
+}
