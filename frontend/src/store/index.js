84ec65ec3f1ce098b92dd04d670fd67390ab6b9e
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-10 17:45:50 +0900

    add: rootStoreにPowerWithStoreを登録

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,21 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import markerStore from '@/store/modules/marker.js'
 import userStore from '@/store/modules/user.js'
+import markerStore from '@/store/modules/marker.js'
 import likeStore from '@/store/modules/spot/like.js'
 import wifiWithStore from '@/store/modules/spot/wifiWith.js'
 import wifiWithoutStore from '@/store/modules/spot/wifiWithout.js'
+import powerWithStore from '@/store/modules/spot/powerWith.js'
 import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    markerStore,
     userStore,
+    markerStore,
     likeStore,
     wifiWithStore,
-    wifiWithoutStore
+    wifiWithoutStore,
+    powerWithStore
   },
 
   plugins: [
