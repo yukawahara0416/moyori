bca76d800c67d5fd38c64cf89e20beb0e7e7ded0
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-11 17:38:01 +0900

    fix: spot関連のモジュールをspotStore配下に移動

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,12 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import userStore from '@/store/modules/user.js'
 import spotStore from '@/store/modules/spot.js'
-import likeStore from '@/store/modules/spot/like.js'
-import wifiWithStore from '@/store/modules/spot/wifiWith.js'
-import wifiWithoutStore from '@/store/modules/spot/wifiWithout.js'
-import powerWithStore from '@/store/modules/spot/powerWith.js'
-import powerWithoutStore from '@/store/modules/spot/powerWithout.js'
-import commentStore from '@/store/modules/spot/comment.js'
+
 import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
@@ -15,13 +10,7 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   modules: {
     userStore,
-    spotStore,
-    likeStore,
-    wifiWithStore,
-    wifiWithoutStore,
-    powerWithStore,
-    powerWithoutStore,
-    commentStore
+    spotStore
   },
 
   plugins: [
--- frontend/src/store/modules/spot.js
+++ frontend/src/store/modules/spot.js
@@ -9,7 +9,23 @@ const axiosBase = axios.create({
   responseType: 'json'
 })
 
+import likeStore from '@/store/modules/spot/like.js'
+import wifiWithStore from '@/store/modules/spot/wifiWith.js'
+import wifiWithoutStore from '@/store/modules/spot/wifiWithout.js'
+import powerWithStore from '@/store/modules/spot/powerWith.js'
+import powerWithoutStore from '@/store/modules/spot/powerWithout.js'
+import commentStore from '@/store/modules/spot/comment.js'
+
 export default {
+  modules: {
+    likeStore,
+    wifiWithStore,
+    wifiWithoutStore,
+    powerWithStore,
+    powerWithoutStore,
+    commentStore
+  },
+
   state: {
     spots: [],
     cache: { id: -1, icon: '' }
