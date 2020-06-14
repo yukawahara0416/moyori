f3b966350b90d3c6f9672264b71807b6175e68cd
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-14 13:42:34 +0900

    improve: DOM修正 router-viewをcontainerに内包させる

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <v-app id="inspire">
-    <v-content class="pa-0" fluid>
-      <router-view />
+    <v-content>
+      <v-container class="pa-0" fluid>
+        <router-view />
+      </v-container>
     </v-content>
   </v-app>
 </template>
