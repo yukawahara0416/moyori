72480da1ca3ab4e36c5986572182b7c9262fc4a4
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-16 12:23:03 +0900

    add: CardIndexを追加

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -1,25 +1,82 @@
 <template>
-  <div>
-    <h1>this is Search</h1>
-    <sign-in />
-    <sign-up />
+  <v-row
+    class="ma-0"
+    fill-height
+    no-gutter
+    style="position: relative; height: calc(100vh - 60px);"
+  >
     <google-maps />
-    <Card />
-  </div>
+
+    <v-col
+      class="pa-0"
+      :class="{
+        indexHeight_big: $vuetify.breakpoint.mdAndUp,
+        indexHeight_small: $vuetify.breakpoint.smAndDown
+      }"
+      cols="12"
+      md="6"
+      no-gutter
+    >
+      <v-container
+        class="overflow-y-auto"
+        fluid
+        id="scroll-target"
+        style="height: 100%; width: 100%;"
+      >
+        <v-row
+          align="center"
+          class="mx-0"
+          justify="center"
+          style="height: calc(100% - 20px); background-color: white;"
+          v-if="!spots.length"
+        >
+          <v-col cols="10">
+            <p>キーワード検索できます</p>
+            <p>現在地に戻ることができます</p>
+            <p>地図のサークル内のカフェを検索できます</p>
+            <v-img
+              class="white--text align-end"
+              :src="require('@/assets/search_hint.png')"
+            />
+          </v-col>
+        </v-row>
+
+        <Card />
+      </v-container>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import GoogleMaps from '@/components/GoogleMaps.vue'
 import Card from '@/components/Card.vue'
-import SignIn from '@/components/SignIn.vue'
-import SignUp from '@/components/SignUp.vue'
 
 export default {
   components: {
     GoogleMaps,
-    Card,
-    SignIn,
-    SignUp
+    Card
+  },
+
+  computed: {
+    ...mapGetters(['spots']),
+
+    vhHeight() {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs':
+          return '60vh'
+        case 'sm':
+          return '70vh'
+        case 'md':
+          return '80vh'
+        case 'lg':
+          return '90vh'
+        case 'xl':
+          return '100vh'
+        default:
+          return '100vh'
+      }
+    }
   }
 }
 </script>
