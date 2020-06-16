32b109fdc9d1030f939f9fba882d2757fdded4a0
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-16 15:56:38 +0900

    add: snackbar表示まで成功

--- frontend/src/components/Snackbar.vue
+++ frontend/src/components/Snackbar.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <v-snackbar>
+  <div class="text-right">
+    <v-snackbar :color="color" top v-model="snackbarState">
       {{ message }}
+      <v-btn @click.native="snackbarState = false" color="white" text>
+        Close
+      </v-btn>
     </v-snackbar>
   </div>
 </template>
@@ -10,8 +13,37 @@
 export default {
   data() {
     return {
-      message: ''
+      color: '',
+      message: '',
+      snackbarState: false
     }
+  },
+
+  created() {
+    this.$store.watch(
+      state => {
+        return state.snackbarStore.color
+      },
+      newVal => {
+        var color = newVal
+        if (color !== '') {
+          this.color = color
+        }
+      }
+    )
+    this.$store.watch(
+      state => {
+        return state.snackbarStore.message
+      },
+      newVal => {
+        var message = newVal
+        if (message !== '') {
+          this.message = message
+          this.snackbarState = true
+          this.$store.dispatch('clearSnackMessage')
+        }
+      }
+    )
   }
 }
 </script>
