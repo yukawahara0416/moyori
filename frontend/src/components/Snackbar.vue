716c746c4a41805f9698e4d70e99f8cbe9881010
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-16 15:08:20 +0900

    add: snackbarコンポーネントを新規作成

---
+++ frontend/src/components/Snackbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-snackbar>
+      {{ message }}
+    </v-snackbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: ''
+    }
+  }
+}
+</script>
+
+<style></style>
