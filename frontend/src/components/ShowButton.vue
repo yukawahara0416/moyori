5143738a604604116991f9c80006cb7ab6ed3dc6
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-06-22 12:54:12 +0900

    add: ShowButtonの基本テンプレを生成

---
+++ frontend/src/components/ShowButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" width="600">
+      <template v-slot:activator="{ on }">
+        <v-btn v-on="on">スポット詳細</v-btn>
+      </template>
+      <v-card>
+        <p>ppppp</p>
+        <p>{{ spot.marker.name }}</p>
+        <p>{{ spot.marker.address }}</p>
+        <p>{{ spot.marker.phone }}</p>
+        <p>{{ spot.marker.website }}</p>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    spot: Object,
+    id: Number
+  },
+
+  data() {
+    return {
+      dialog: false
+    }
+  }
+}
+</script>
+
+<style></style>
