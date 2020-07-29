e715878bbfbede7287b1bcc88987d9fbf49a62a8
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-07-30 08:57:32 +0900

    add: 単体テスト ProfileActionsItemsImage

---
+++ frontend/tests/unit/components/Profile/ProfileActionsItemsImage.spec.js
@@ -0,0 +1,31 @@
+import { shallowMount } from '@vue/test-utils'
+import Component from '@/components/Profile/ProfileActionsItemsImage.vue'
+
+let wrapper
+let propsData
+
+beforeEach(() => {
+  propsData = {
+    user: { data: { id: 1 }, posts: [{ data: { id: 1 } }] }
+  }
+
+  wrapper = shallowMount(Component, {
+    propsData
+  })
+})
+
+afterEach(() => {
+  wrapper.destroy()
+})
+
+describe('props', () => {
+  it('user', () => {
+    expect(wrapper.props().user).toEqual(propsData.user)
+  })
+})
+
+describe('template', () => {
+  it('snapshot', () => {
+    expect(wrapper.vm.$el).toMatchSnapshot()
+  })
+})
