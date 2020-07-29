79da368c0c3262ffdcb86389b0830aea640f0b9f
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-07-29 12:39:29 +0900

    add: 新規component を生成し、親コンポーネントから分離
    リファクタリング

---
+++ frontend/src/components/Header/HeaderAvatarImage.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-avatar class="white--text headline" color="success" size="36">
+    <img alt="" src="" v-if="currentUser.data.image" />
+    <span class="white--text headline" v-else>
+      {{ currentUser.data.name.slice(0, 1) }}
+    </span>
+  </v-avatar>
+</template>
+
+<script>
+export default {
+  props: {
+    currentUser: Object
+  }
+}
+</script>
+
+<style></style>
