f249a74ee49cc5018e72d72459c25b2f09c510f7
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-07-29 12:45:30 +0900

    add: 新規componentを生成し、親から分離
    リファクタリング

---
+++ frontend/src/components/Header/HeaderDrawerFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-list-item-group>
+    <v-list-item class="text-center" disabled>
+      <v-list-item-subtitle>
+        &copy; 2020
+        <strong>MoYoRi</strong>
+        <br />
+        All Rights Reserved.
+      </v-list-item-subtitle>
+    </v-list-item>
+  </v-list-item-group>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
