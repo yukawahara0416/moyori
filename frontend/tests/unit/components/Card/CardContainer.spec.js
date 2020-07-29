72fffb3d667eb8cdbcc53ba4ceb60c887b045273
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-07-29 15:32:09 +0900

    add: 単体テスト CardContainer

---
+++ frontend/tests/unit/components/Card/CardContainer.spec.js
@@ -0,0 +1,41 @@
+import { shallowMount } from '@vue/test-utils'
+import Component from '@/components/Card/CardContainer.vue'
+
+let wrapper
+let propsData
+
+beforeEach(() => {
+  propsData = {
+    spots: [{ data: { id: 1 } }, { data: { id: 2 } }],
+    type: 'map'
+  }
+
+  wrapper = shallowMount(Component, {
+    propsData
+  })
+})
+
+afterEach(() => {
+  wrapper.destroy()
+})
+
+describe('props', () => {
+  it('spots', () => {
+    expect(wrapper.props().spots).toStrictEqual(propsData.spots)
+    expect(wrapper.props().spots instanceof Array).toBe(true)
+  })
+  it('type', () => {
+    expect(wrapper.props().type).toStrictEqual(propsData.type)
+    expect(typeof wrapper.vm.$props.type).toBe('string')
+  })
+})
+
+// describe('computed', () => {
+//   it('empties', () => {})
+// })
+
+describe('template', () => {
+  it('snapshot', () => {
+    expect(wrapper.vm.$el).toMatchSnapshot()
+  })
+})
