2f239459c70e3248d9c91804f79161557dd30564
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-07-29 12:40:28 +0900

    add: 新規component を生成し、親コンポーネントから分離
    リファクタリング

---
+++ frontend/src/components/Header/HeaderAvatarList.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-list>
+    <v-list-item>{{ currentUser.data.name }} さん</v-list-item>
+    <header-avatar-list-mypage :currentUser="currentUser" />
+    <header-avatar-list-signout />
+  </v-list>
+</template>
+
+<script>
+import HeaderAvatarListMypage from '@/components/Header/HeaderAvatarListMypage.vue'
+import HeaderAvatarListSignout from '@/components/Header/HeaderAvatarListSignout.vue'
+
+export default {
+  props: {
+    currentUser: Object
+  },
+
+  components: {
+    HeaderAvatarListMypage,
+    HeaderAvatarListSignout
+  }
+}
+</script>
+
+<style></style>
---
+++ frontend/src/components/Header/HeaderAvatarListSignout.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-list-item @click="signOut">
+    <v-list-item-avatar>
+      <v-icon btn>mdi-logout</v-icon>
+    </v-list-item-avatar>
+    <v-list-item-content>
+      <v-list-item-title>ログアウト</v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  methods: {
+    signOut() {
+      this.$store.dispatch('signOut')
+    }
+  }
+}
+</script>
+
+<style></style>
