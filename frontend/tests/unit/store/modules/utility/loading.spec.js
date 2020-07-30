8f68799c9350c8add48e357217aa3f5a6ea5d3d0
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-07-30 12:28:59 +0900

    add: 単体テスト vuex loading.js

---
+++ frontend/tests/unit/store/modules/utility/loading.spec.js
@@ -0,0 +1,46 @@
+import { createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import loading from '@/store/modules/utility/loading.js'
+import { cloneDeep } from 'lodash'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+let store
+
+beforeEach(() => {
+  store = new Vuex.Store(cloneDeep(loading))
+})
+
+describe('getters', () => {
+  it('loading', () => {
+    store.replaceState({ loading: true })
+    expect(store.getters['loading']).toBe(true)
+  })
+})
+
+describe('mutations', () => {
+  it('loadingOn', () => {
+    store.commit('loadingOn')
+    expect(store.state.loading).toBe(true)
+  })
+  it('loadingOff', () => {
+    store.replaceState({ loading: true })
+    store.commit('loadingOff')
+    expect(store.state.loading).toBe(false)
+  })
+})
+
+describe('actions', () => {
+  it('loadingOn', () => {
+    store.dispatch('loadingOn').then(res => {
+      expect(store.state.loading).toBe(true)
+    })
+  })
+  it('loadingOff', () => {
+    store.replaceState({ loading: true })
+    store.dispatch('loadingOff').then(res => {
+      expect(store.state.loading).toBe(false)
+    })
+  })
+})
