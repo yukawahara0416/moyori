6d612334f152441ed9a1f3fa9707522531a1946d
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-07-31 14:38:44 +0900

    add: デモ用ボタンを仮配置

--- frontend/src/components/Map/MapContainerToolbar.vue
+++ frontend/src/components/Map/MapContainerToolbar.vue
@@ -5,6 +5,14 @@
       <map-container-toolbar-nearby-button @nearby-search="nearbySearch" />
       <map-container-toolbar-panto-button @panto-location="panToLocation" />
     </v-toolbar>
+
+    <v-toolbar style="position: absolute; left: 10px; top: 90px; z-index: 2;">
+      <v-btn class="mx-2">
+        <v-icon class="mr-2">mdi-database</v-icon>
+        デモ用エリアへ移動
+        <span style="font-size: 5px;">（実用データが豊富です^^）</span>
+      </v-btn>
+    </v-toolbar>
   </div>
 </template>
 
@@ -12,7 +20,6 @@
 // import MapContainerToolbarTextForm from '@/components/Map/MapContainerToolbarTextForm.vue'
 import MapContainerToolbarPantoButton from '@/components/Map/MapContainerToolbarPantoButton.vue'
 import MapContainerToolbarNearbyButton from '@/components/Map/MapContainerToolbarNearbyButton.vue'
-import MapContainerToolbarDemoButton from '@/components/Map/MapContainerToolbarDemoButton.vue'
 
 export default {
   components: {
