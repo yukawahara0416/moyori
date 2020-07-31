3405f4dc4c138df7512b59c99cfad63095a07285
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-07-31 14:36:21 +0900

    improve: テキスト検索フォームをコメントアウト
    現在使う予定がないため

--- frontend/src/components/Map/MapContainerToolbar.vue
+++ frontend/src/components/Map/MapContainerToolbar.vue
@@ -1,29 +1,30 @@
 <template>
-  <v-toolbar style="position: absolute; left: 10px; top: 10px; z-index: 2;">
-    <map-container-toolbar-text-form @text-search="textSearch" />
-
-    <map-container-toolbar-panto-button @panto-location="panToLocation" />
-
-    <map-container-toolbar-nearby-button @nearby-search="nearbySearch" />
-  </v-toolbar>
+  <div>
+    <v-toolbar style="position: absolute; left: 10px; top: 10px; z-index: 2;">
+      <!-- <map-container-toolbar-text-form @text-search="textSearch" /> -->
+      <map-container-toolbar-nearby-button @nearby-search="nearbySearch" />
+      <map-container-toolbar-panto-button @panto-location="panToLocation" />
+    </v-toolbar>
+  </div>
 </template>
 
 <script>
-import MapContainerToolbarTextForm from '@/components/Map/MapContainerToolbarTextForm.vue'
+// import MapContainerToolbarTextForm from '@/components/Map/MapContainerToolbarTextForm.vue'
 import MapContainerToolbarPantoButton from '@/components/Map/MapContainerToolbarPantoButton.vue'
 import MapContainerToolbarNearbyButton from '@/components/Map/MapContainerToolbarNearbyButton.vue'
+import MapContainerToolbarDemoButton from '@/components/Map/MapContainerToolbarDemoButton.vue'
 
 export default {
   components: {
-    MapContainerToolbarTextForm,
+    // MapContainerToolbarTextForm,
     MapContainerToolbarPantoButton,
     MapContainerToolbarNearbyButton
   },
 
   methods: {
-    textSearch(keyword) {
-      this.$emit('text-search', keyword)
-    },
+    // textSearch(keyword) {
+    //   this.$emit('text-search', keyword)
+    // },
 
     panToLocation() {
       this.$emit('panto-location')
