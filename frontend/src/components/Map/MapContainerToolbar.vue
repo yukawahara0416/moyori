1ddd2966bd76919ab0611e865279f41722d724c1
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-08-02 08:59:40 +0900

    improve: 新規コンポーネントdemoSearchを生成し分離

--- frontend/src/components/Map/MapContainerToolbar.vue
+++ frontend/src/components/Map/MapContainerToolbar.vue
@@ -2,16 +2,9 @@
   <div>
     <v-toolbar style="position: absolute; left: 10px; top: 10px; z-index: 2;">
       <!-- <map-container-toolbar-text-form @text-search="textSearch" /> -->
+      <map-container-toolbar-demo-button @demo-search="demoSearch" />
       <map-container-toolbar-nearby-button @nearby-search="nearbySearch" />
       <map-container-toolbar-panto-button @panto-location="panToLocation" />
-    </v-toolbar>
-
-    <v-toolbar style="position: absolute; left: 10px; top: 90px; z-index: 2;">
-      <v-btn class="mx-2">
-        <v-icon class="mr-2">mdi-database</v-icon>
-        デモ用エリアへ移動
-        <span style="font-size: 5px;">（実用データが豊富です^^）</span>
-      </v-btn>
     </v-toolbar>
   </div>
 </template>
@@ -20,12 +13,14 @@
 // import MapContainerToolbarTextForm from '@/components/Map/MapContainerToolbarTextForm.vue'
 import MapContainerToolbarPantoButton from '@/components/Map/MapContainerToolbarPantoButton.vue'
 import MapContainerToolbarNearbyButton from '@/components/Map/MapContainerToolbarNearbyButton.vue'
+import MapContainerToolbarDemoButton from '@/components/Map/MapContainerToolbarDemoButton.vue'
 
 export default {
   components: {
     // MapContainerToolbarTextForm,
     MapContainerToolbarPantoButton,
-    MapContainerToolbarNearbyButton
+    MapContainerToolbarNearbyButton,
+    MapContainerToolbarDemoButton
   },
 
   methods: {
@@ -39,6 +34,10 @@ export default {
 
     nearbySearch() {
       this.$emit('nearby-search')
+    },
+
+    demoSearch() {
+      this.$emit('demo-search')
     }
   }
 }
