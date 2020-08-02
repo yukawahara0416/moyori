33af44e89edbcf47a406d9f232aeb26dcd501cbe
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-08-03 06:28:27 +0900

    add: テスト用オプションボタンを追加

--- frontend/src/components/Map/MapContainerToolbarOptionButton.vue
+++ frontend/src/components/Map/MapContainerToolbarOptionButton.vue
@@ -1,4 +1,18 @@
-<template></template>
+<template>
+  <v-menu open-on-hover offset-y>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn icon class="mx-2" v-bind="attrs" v-on="on">
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </template>
+
+    <v-list>
+      <v-list-item>いち</v-list-item>
+      <v-list-item>に</v-list-item>
+      <v-list-item>さん</v-list-item>
+    </v-list>
+  </v-menu>
+</template>
 
 <script>
 export default {}
