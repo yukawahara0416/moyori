f8fdb53e4d8a46c0f15f0fe79954ce228827e782
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-08-03 06:26:57 +0900

    add: 新規コンポーネントMapContainerToolbarOptionButtonを生成

--- frontend/src/components/Map/MapContainerToolbar.vue
+++ frontend/src/components/Map/MapContainerToolbar.vue
@@ -8,6 +8,7 @@
       <map-container-toolbar-demo-button @demo-search="demoSearch" />
       <map-container-toolbar-nearby-button @nearby-search="nearbySearch" />
       <map-container-toolbar-panto-button @panto-location="panToLocation" />
+      <map-container-toolbar-option-button />
     </v-toolbar>
   </div>
 </template>
@@ -17,13 +18,15 @@
 import MapContainerToolbarPantoButton from '@/components/Map/MapContainerToolbarPantoButton.vue'
 import MapContainerToolbarNearbyButton from '@/components/Map/MapContainerToolbarNearbyButton.vue'
 import MapContainerToolbarDemoButton from '@/components/Map/MapContainerToolbarDemoButton.vue'
+import MapContainerToolbarOptionButton from '@/components/Map/MapContainerToolbarOptionButton.vue'
 
 export default {
   components: {
     // MapContainerToolbarTextForm,
     MapContainerToolbarPantoButton,
     MapContainerToolbarNearbyButton,
-    MapContainerToolbarDemoButton
+    MapContainerToolbarDemoButton,
+    MapContainerToolbarOptionButton
   },
 
   methods: {
