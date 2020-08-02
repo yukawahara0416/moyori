3d4c34742d420e329adc7b0e6191c869e0578ea1
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-08-02 09:00:07 +0900

    add: 新規コンポーネントdemoSearchを生成し親より移動

---
+++ frontend/src/components/Map/MapContainerToolbarDemoButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-btn class="mx-2 success" @click="demoSearch">
+    <v-icon class="mr-2">mdi-database</v-icon>
+    デモ用エリアへ移動
+  </v-btn>
+</template>
+
+<script>
+export default {
+  methods: {
+    demoSearch() {
+      this.$emit('demo-search')
+    }
+  }
+}
+</script>
+
+<style></style>
