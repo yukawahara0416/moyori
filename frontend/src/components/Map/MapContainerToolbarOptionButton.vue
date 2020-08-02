bf5c3d88346b9d07d75049bdb3c970738cd6df00
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-08-03 07:08:05 +0900

    add: スイッチを追加

--- frontend/src/components/Map/MapContainerToolbarOptionButton.vue
+++ frontend/src/components/Map/MapContainerToolbarOptionButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-menu open-on-hover offset-y>
+  <v-menu offset-y open-on-hover :close-on-content-click="false">
     <template v-slot:activator="{ on, attrs }">
       <v-btn icon class="mx-2" v-bind="attrs" v-on="on">
         <v-icon>mdi-filter-menu-outline</v-icon>
@@ -7,15 +7,45 @@
     </template>
 
     <v-list>
-      <v-list-item>いち</v-list-item>
-      <v-list-item>に</v-list-item>
-      <v-list-item>さん</v-list-item>
+      <v-list-item>
+        <v-list-item-action>
+          <v-switch v-model="like" color="success"></v-switch>
+        </v-list-item-action>
+        <v-list-item-title>いいねあり</v-list-item-title>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-action>
+          <v-switch v-model="wifi" color="success"></v-switch>
+        </v-list-item-action>
+        <v-list-item-title>WiFiあり</v-list-item-title>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-action>
+          <v-switch v-model="power" color="success"></v-switch>
+        </v-list-item-action>
+        <v-list-item-title>電源あり</v-list-item-title>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-action>
+          <v-switch v-model="comment" color="success"></v-switch>
+        </v-list-item-action>
+        <v-list-item-title>コメントあり</v-list-item-title>
+      </v-list-item>
     </v-list>
   </v-menu>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      like: false,
+      wifi: false,
+      power: false,
+      comment: false
+    }
+  }
+}
 </script>
 
 <style></style>
