8f35df757d346b9fb82cd0f6b73dc61fc7fa3d14
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-08-04 16:35:13 +0900

    improve: フィルターに使用するクエリのdata定義を修正

--- frontend/src/components/Map/MapContainerToolbarOptionButton.vue
+++ frontend/src/components/Map/MapContainerToolbarOptionButton.vue
@@ -9,27 +9,42 @@
     <v-list>
       <v-list-item>
         <v-list-item-action>
-          <v-switch v-model="like" color="success"></v-switch>
+          <v-switch color="success" v-model="filterQuery.likes"></v-switch>
         </v-list-item-action>
-        <v-list-item-title>いいねあり</v-list-item-title>
+        <v-list-item-title>
+          <v-icon class="mr-2">mdi-heart-outline</v-icon>
+          いいね あり
+        </v-list-item-title>
       </v-list-item>
       <v-list-item>
         <v-list-item-action>
-          <v-switch v-model="wifi" color="success"></v-switch>
+          <v-switch color="success" v-model="filterQuery.wifi_withs"></v-switch>
         </v-list-item-action>
-        <v-list-item-title>WiFiあり</v-list-item-title>
+        <v-list-item-title>
+          <v-icon class="mr-2">mdi-wifi</v-icon>
+          WiFiあり
+        </v-list-item-title>
       </v-list-item>
       <v-list-item>
         <v-list-item-action>
-          <v-switch v-model="power" color="success"></v-switch>
+          <v-switch
+            color="success"
+            v-model="filterQuery.power_withs"
+          ></v-switch>
         </v-list-item-action>
-        <v-list-item-title>電源あり</v-list-item-title>
+        <v-list-item-title>
+          <v-icon class="mr-2">mdi-power-plug</v-icon>
+          電源あり
+        </v-list-item-title>
       </v-list-item>
       <v-list-item>
         <v-list-item-action>
-          <v-switch v-model="comment" color="success"></v-switch>
+          <v-switch color="success" v-model="filterQuery.comments"></v-switch>
         </v-list-item-action>
-        <v-list-item-title>コメントあり</v-list-item-title>
+        <v-list-item-title>
+          <v-icon class="mr-2">mdi-message-outline</v-icon>
+          コメントあり
+        </v-list-item-title>
       </v-list-item>
     </v-list>
   </v-menu>
@@ -39,10 +54,12 @@
 export default {
   data() {
     return {
-      like: false,
-      wifi: false,
-      power: false,
-      comment: false
+      filterQuery: {
+        likes: false,
+        wifi_withs: false,
+        power_withs: false,
+        comments: false
+      }
     }
   }
 }
