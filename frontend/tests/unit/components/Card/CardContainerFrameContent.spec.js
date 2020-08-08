3f5971d9664ff809117e6d6e97fb48f3c48535ef
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-08-09 06:10:56 +0900

    fix: props/id削除にともない該当するテストを削除

--- frontend/tests/unit/components/Card/CardContainerFrameContent.spec.js
+++ frontend/tests/unit/components/Card/CardContainerFrameContent.spec.js
@@ -25,10 +25,6 @@ describe('proprs', () => {
     expect(wrapper.props().spot).toStrictEqual(propsData.spot)
     expect(wrapper.props().spot instanceof Object).toBe(true)
   })
-  it('id', () => {
-    expect(wrapper.props().id).toStrictEqual(propsData.id)
-    expect(typeof wrapper.vm.$props.id).toBe('number')
-  })
   it('type', () => {
     expect(wrapper.props().type).toStrictEqual(propsData.type)
     expect(typeof wrapper.vm.$props.type).toBe('string')
