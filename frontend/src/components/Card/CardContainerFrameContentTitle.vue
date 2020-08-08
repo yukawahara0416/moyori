95eb5e3513b6823a256eeb755f23962e0007449a
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-08-09 08:43:57 +0900

    add: card-title用のSFCを新規作成

---
+++ frontend/src/components/Card/CardContainerFrameContentTitle.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card-title class="nowrap">
+    <p>{{ spot.marker.name }}</p>
+  </v-card-title>
+</template>
+
+<script>
+export default {
+  props: {
+    spot: Object
+  }
+}
+</script>
+
+<style scoped>
+.nowrap p {
+  white-space: nowrap;
+  overflow: hidden;
+}
+</style>
