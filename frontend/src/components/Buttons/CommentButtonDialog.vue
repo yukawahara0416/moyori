7b9321e6c4392ce6ce5c359b9b8b63b138d415b3
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-08-09 09:40:56 +0900

    add: SFC CommentButtonDialogを新規作成

---
+++ frontend/src/components/Buttons/CommentButtonDialog.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card>
+    <v-form @submit.prevent>
+      <v-text-field name="comment" type="text" v-model="content" />
+    </v-form>
+
+    <v-btn @click="commentHandler()" type="submit">コメント</v-btn>
+
+    <div v-for="(c, id) in spot.comments" :key="id">
+      <p>{{ c.content }}</p>
+      <v-btn
+        type="submit"
+        v-if="c.user_id == currentUser.data.id"
+        @click="deleteComment(c, id)"
+      ></v-btn>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
