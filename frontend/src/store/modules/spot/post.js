46ff786603ae7fbdcabbcda44d92088d528e35a3
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-08-10 08:44:44 +0900

    add: スポット投稿キャンセル機能を実装

--- frontend/src/store/modules/spot/post.js
+++ frontend/src/store/modules/spot/post.js
@@ -98,6 +98,19 @@ export default {
         })
     },
 
+    cancelPostSpot(context) {
+      context.dispatch('dialogOff', null, { root: true })
+      context.dispatch('clearSpotFormData')
+      context.dispatch(
+        'pushSnackbar',
+        {
+          message: 'スポットを登録をキャンセルしました',
+          color: 'error'
+        },
+        { root: true }
+      )
+    },
+
     geocode(context, event) {
       const geocoder = new google.maps.Geocoder()
       var latLng = {
