764ef0823ded453474ecb7c9b55ffe96678499e6
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-08-16 09:48:14 +0900

    add: スポット詳細ダイヤログに使用する写真スライド用のSFCを新規作成

---
+++ frontend/src/components/Spot/SpotShowDialogImageSlide.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-slide-group class="pa-4" show-arrows>
+    <v-slide-item v-for="(photo, id) in photos" :key="id">
+      <spot-show-dialog-image-slide-dialog :photo="photo" />
+    </v-slide-item>
+  </v-slide-group>
+</template>
+
+<script>
+import SpotShowDialogImageSlideDialog from '@/components/Spot/SpotShowDialogImageSlideDialog.vue'
+
+export default {
+  props: {
+    spot: Object
+  },
+
+  components: {
+    SpotShowDialogImageSlideDialog
+  },
+
+  computed: {
+    photos() {
+      return this.spot.detail.photos
+    }
+  }
+}
+</script>
+
+<style></style>
