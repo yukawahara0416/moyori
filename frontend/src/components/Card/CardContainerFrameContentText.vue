1808720e8a39b7e419373ad858a59f9cb2ccff1a
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-08-16 09:41:25 +0900

    improve: Googleマップでナビボタンを詳細ダイアログへ移動
    カード一覧のみやすさを優先するため

--- frontend/src/components/Card/CardContainerFrameContentText.vue
+++ frontend/src/components/Card/CardContainerFrameContentText.vue
@@ -1,20 +1,6 @@
 <template>
   <v-card-text class="text--primary nowrap">
     <p>{{ spot.marker.address }}</p>
-
-    <p>
-      <a
-        :href="
-          'https://www.google.com/maps/dir/?api=1&destination=' +
-            spot.marker.position.lat +
-            ',' +
-            spot.marker.position.lng
-        "
-        target="_brank"
-      >
-        Googleマップでナビ
-      </a>
-    </p>
   </v-card-text>
 </template>
 
