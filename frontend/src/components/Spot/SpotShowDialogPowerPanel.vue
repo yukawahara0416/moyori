a6ed3144851b091167063c77464eec79e45dc3f4
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-08-21 07:22:14 +0900

    improve: 電源サービス有無の判定に記号だけでなく文章を追加

--- frontend/src/components/Spot/SpotShowDialogPowerPanel.vue
+++ frontend/src/components/Spot/SpotShowDialogPowerPanel.vue
@@ -6,19 +6,34 @@
           <v-icon>mdi-power-plug</v-icon>
           電源サービス：
           <span class="ml-3">
-            <v-icon v-if="analyzeVote == 'excellent'" color="success">
-              mdi-circle-double
-            </v-icon>
-            <v-icon v-else-if="analyzeVote == 'good'" color="success">
-              mdi-circle-outline
-            </v-icon>
-            <v-icon v-else-if="analyzeVote == 'fair'" color="primary">
-              mdi-triangle-outline
-            </v-icon>
-            <v-icon v-else-if="analyzeVote == 'poor'" color="error">
-              mdi-close
-            </v-icon>
-            <v-icon v-else>mdi-help</v-icon>
+            <span v-if="analyzeVote == 'excellent'">
+              <v-icon class="mr-3" color="success">
+                mdi-circle-double
+              </v-icon>
+              <span>（かなり期待できます^_^）</span>
+            </span>
+            <span v-else-if="analyzeVote == 'good'">
+              <v-icon class="mr-3" color="success">
+                mdi-circle-outline
+              </v-icon>
+              <span>（そこそこ期待できます^_^）</span>
+            </span>
+            <span v-else-if="analyzeVote == 'fair'">
+              <v-icon class="mr-3" color="primary">
+                mdi-triangle-outline
+              </v-icon>
+              <span>（もしかしたらなくなってるかも^^;）</span>
+            </span>
+            <span v-else-if="analyzeVote == 'poor'">
+              <v-icon class="mr-3" color="error">
+                mdi-close
+              </v-icon>
+              <span>（なくなってる可能性大です^^;）</span>
+            </span>
+            <span v-else>
+              <v-icon class="mr-3">mdi-help</v-icon>
+              <span>（まだ投票されていません）</span>
+            </span>
           </span>
         </span>
       </v-expansion-panel-header>
@@ -67,15 +82,38 @@
           <span>{{ latestVote ? '電源あるよ' : '電源ないよ' }}</span>
           」です。
         </p>
+
+        <google-chart-bar-chart
+          v-if="
+            spot.power_withs.length !== 0 || spot.power_withouts.length !== 0
+          "
+          :withs="spot.power_withs"
+          :withouts="spot.power_withouts"
+        />
+        <google-chart-area-chart
+          v-if="
+            spot.power_withs.length !== 0 || spot.power_withouts.length !== 0
+          "
+          :withs="spot.power_withs"
+          :withouts="spot.power_withouts"
+        />
       </v-expansion-panel-content>
     </v-expansion-panel>
   </v-expansion-panels>
 </template>
 
 <script>
+import GoogleChartBarChart from '@/components/Chart/GoogleChartBarChart.vue'
+import GoogleChartAreaChart from '@/components/Chart/GoogleChartAreaChart.vue'
+
 export default {
   props: {
     spot: Object
+  },
+
+  components: {
+    GoogleChartBarChart,
+    GoogleChartAreaChart
   },
 
   computed: {
