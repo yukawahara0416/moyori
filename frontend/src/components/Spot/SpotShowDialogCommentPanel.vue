dd1dcb2eef6b63d3a11e6b7236e8427052b63b3a
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-09-11 15:14:01 +0900

    add: スポット詳細のコメント欄用SFCを新規作成

---
+++ frontend/src/components/Spot/SpotShowDialogCommentPanel.vue
@@ -0,0 +1,94 @@
+<template>
+  <v-card>
+    <v-form @submit.prevent>
+      <v-textarea
+        label="コメントを入力してください"
+        name="comment"
+        prepend-icon="mdi-comment"
+        rows="5"
+        type="text"
+        v-model="content"
+      />
+    </v-form>
+
+    <v-btn @click="commentHandler()" type="submit">コメント</v-btn>
+
+    <div v-for="(comment, id) in spot.comments" :key="id">
+      <p>{{ comment.content }}</p>
+      <v-btn
+        type="submit"
+        v-if="currentUser && comment.user_id == currentUser.data.id"
+        @click="deleteComment({ spot, comment, type })"
+      >
+        削除
+      </v-btn>
+    </div>
+  </v-card>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  props: {
+    spot: Object,
+    type: String
+  },
+
+  data() {
+    return {
+      content: ''
+    }
+  },
+
+  computed: {
+    ...mapGetters(['headers', 'currentUser', 'dialogSign']),
+
+    isLoggedIn() {
+      return this.headers !== null ? true : false
+    }
+  },
+
+  // ここからコピペしただけ手つかず
+  methods: {
+    ...mapActions({ saveSpot: 'map/saveSpot' }),
+    ...mapActions(['postComment', 'deleteComment', 'pushSnackbar']),
+
+    commentHandler: async function() {
+      const spot = this.spot
+      const type = this.type
+      const isPosted = Object.prototype.hasOwnProperty.call(
+        this.spot.data,
+        'id'
+      )
+      if (this.isLoggedIn) {
+        if (isPosted) {
+          await this.postComment({
+            spot: spot,
+            type: type,
+            content: this.content
+          })
+          this.content = ''
+        } else {
+          const result = await this.saveSpot({ spot: spot })
+          await this.postComment({
+            spot: result,
+            type: type,
+            content: this.content
+          })
+          this.content = ''
+        }
+      } else {
+        this.dialogOn()
+        this.pushSnackbar({ message: 'ログインしてください', color: 'error' })
+      }
+    },
+
+    dialogOn() {
+      this.$store.dispatch('dialogOn', 'dialogSign')
+    }
+  }
+}
+</script>
+
+<style></style>
