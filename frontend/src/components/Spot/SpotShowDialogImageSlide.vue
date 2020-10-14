c030d4c5fd6e0bd943b9dc72e47cd08de651997b
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-10-14 12:36:43 +0900

    improve: スポット詳細ダイアログの各項目をv-card化

--- frontend/src/components/Spot/SpotShowDialogImageSlide.vue
+++ frontend/src/components/Spot/SpotShowDialogImageSlide.vue
@@ -1,9 +1,19 @@
 <template>
-  <v-slide-group class="pa-4" show-arrows>
-    <v-slide-item v-for="(photo, id) in photos" :key="id">
-      <spot-show-dialog-image-slide-dialog :photo="photo" />
-    </v-slide-item>
-  </v-slide-group>
+  <v-card flat outlined class="mb-2 py-2">
+    <v-slide-group show-arrows>
+      <v-slide-item>
+        <v-card class="d-flex mx-2" flat outlined tile width="100px">
+          <v-img
+            aspect-ratio="1"
+            :src="require('@/assets/noimage.png')"
+          ></v-img>
+        </v-card>
+      </v-slide-item>
+      <v-slide-item v-for="(photo, id) in photos" :key="id">
+        <spot-show-dialog-image-slide-dialog :photo="photo" />
+      </v-slide-item>
+    </v-slide-group>
+  </v-card>
 </template>
 
 <script>
---
+++ frontend/src/components/Spot/SpotShowDialogInfoPanel.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-card flat outlined class="mb-2">
+    <v-card-text>
+      <spot-show-dialog-info-panel-address :spot="spot" />
+      <spot-show-dialog-info-panel-phone :spot="spot" />
+      <spot-show-dialog-info-panel-website :spot="spot" />
+      <spot-show-dialog-info-panel-business :spot="spot" />
+    </v-card-text>
+    <v-card-actions v-if="isOwnPosted">
+      <v-spacer />
+      <spot-edit-dialog :spot="spot" />
+      <v-spacer />
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import SpotShowDialogInfoPanelAddress from '@/components/Spot/SpotShowDialogInfoPanelAddress.vue'
+import SpotShowDialogInfoPanelPhone from '@/components/Spot/SpotShowDialogInfoPanelPhone.vue'
+import SpotShowDialogInfoPanelWebsite from '@/components/Spot/SpotShowDialogInfoPanelWebsite.vue'
+import SpotShowDialogInfoPanelBusiness from '@/components/Spot/SpotShowDialogInfoPanelBusiness.vue'
+import SpotEditDialog from '@/components/Spot/SpotEditDialog.vue'
+
+export default {
+  props: {
+    spot: Object
+  },
+
+  components: {
+    SpotShowDialogInfoPanelAddress,
+    SpotShowDialogInfoPanelPhone,
+    SpotShowDialogInfoPanelWebsite,
+    SpotShowDialogInfoPanelBusiness,
+    SpotEditDialog
+  },
+
+  computed: {
+    ...mapGetters(['headers', 'currentUser']),
+
+    isLoggedIn() {
+      return this.headers !== null ? true : false
+    },
+
+    isOwnPosted() {
+      if (this.isLoggedIn) {
+        if (this.spot.detail.formatted_address) {
+          return false
+        } else if (this.spot.data.user_id !== this.currentUser.data.id) {
+          return false
+        } else {
+          return true
+        }
+      } else {
+        return false
+      }
+    }
+  }
+}
+</script>
+
+<style></style>
