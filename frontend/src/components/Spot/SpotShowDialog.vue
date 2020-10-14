d0262fca7e6346f672793ef5a9bccb17eb6893b9
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-10-14 12:34:34 +0900

    rename: SFC階層変更に伴いリネーム

--- frontend/src/components/Spot/SpotShowDialog.vue
+++ frontend/src/components/Spot/SpotShowDialog.vue
@@ -15,35 +15,21 @@
     </v-card-title>
 
     <v-card-text>
-      <spot-show-dialog-address :spot="spot" />
-      <spot-show-dialog-phone :spot="spot" />
-      <spot-show-dialog-website :spot="spot" />
-      <spot-show-dialog-business-panel :spot="spot" />
+      <spot-show-dialog-info-panel :spot="spot" />
+      <spot-show-dialog-image-slide :spot="spot" />
       <spot-show-dialog-wifi-panel :spot="spot" />
       <spot-show-dialog-power-panel :spot="spot" />
-      <spot-show-dialog-image-slide :spot="spot" />
       <spot-show-dialog-comment-panel :spot="spot" :type="type" />
     </v-card-text>
-
-    <v-card-actions v-if="isOwnPosted">
-      <v-spacer />
-      <spot-edit-dialog :spot="spot" />
-      <v-spacer />
-    </v-card-actions>
   </v-card>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import SpotShowDialogAddress from '@/components/Spot/SpotShowDialogAddress.vue'
-import SpotShowDialogPhone from '@/components/Spot/SpotShowDialogPhone.vue'
-import SpotShowDialogWebsite from '@/components/Spot/SpotShowDialogWebsite.vue'
-import SpotShowDialogBusinessPanel from '@/components/Spot/SpotShowDialogBusinessPanel.vue'
+import SpotShowDialogInfoPanel from '@/components/Spot/SpotShowDialogInfoPanel.vue'
 import SpotShowDialogWifiPanel from '@/components/Spot/SpotShowDialogWifiPanel.vue'
 import SpotShowDialogPowerPanel from '@/components/Spot/SpotShowDialogPowerPanel.vue'
 import SpotShowDialogImageSlide from '@/components/Spot/SpotShowDialogImageSlide.vue'
 import SpotShowDialogCommentPanel from '@/components/Spot/SpotShowDialogCommentPanel.vue'
-import SpotEditDialog from '@/components/Spot/SpotEditDialog.vue'
 
 export default {
   props: {
@@ -53,37 +39,11 @@ export default {
   },
 
   components: {
-    SpotShowDialogAddress,
-    SpotShowDialogPhone,
-    SpotShowDialogWebsite,
-    SpotShowDialogBusinessPanel,
+    SpotShowDialogInfoPanel,
     SpotShowDialogWifiPanel,
     SpotShowDialogPowerPanel,
     SpotShowDialogImageSlide,
-    SpotShowDialogCommentPanel,
-    SpotEditDialog
-  },
-
-  computed: {
-    ...mapGetters(['headers', 'currentUser']),
-
-    isLoggedIn() {
-      return this.headers !== null ? true : false
-    },
-
-    isOwnPosted() {
-      if (this.isLoggedIn) {
-        if (this.spot.detail.formatted_address) {
-          return false
-        } else if (this.spot.data.user_id !== this.currentUser.data.id) {
-          return false
-        } else {
-          return true
-        }
-      } else {
-        return false
-      }
-    }
+    SpotShowDialogCommentPanel
   },
 
   methods: {
---
+++ frontend/src/components/Spot/SpotShowDialogInfoPanelBusiness.vue
@@ -0,0 +1,27 @@
+<template>
+  <span>
+    <p v-if="spot.detail.opening_hours">
+      営業時間：
+      <span class="ml-3" v-for="(day, id) in businessDays" :key="id">
+        {{ day }}
+      </span>
+    </p>
+    <p v-else>営業時間：</p>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    spot: Object
+  },
+
+  computed: {
+    businessDays() {
+      return this.spot.detail.opening_hours.weekday_text
+    }
+  }
+}
+</script>
+
+<style></style>
