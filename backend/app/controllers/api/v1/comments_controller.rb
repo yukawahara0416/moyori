ca46be5ab8676c58aed94d521abaf191ffa9e73d
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-10-24 05:25:24 +0900

    improve: commentのJSONにcomment.imageを追加

--- backend/app/controllers/api/v1/comments_controller.rb
+++ backend/app/controllers/api/v1/comments_controller.rb
@@ -4,8 +4,8 @@ module Api
       before_action :authenticate_api_v1_user!, only: %i[create destroy]
 
       def create
-        current_api_v1_user.comments.create!(comments_params)
-        render json: convert_to_json_comment
+        @comment = current_api_v1_user.comments.create!(comments_params)
+        render json: convert_to_json_comment(@comment)
       end
 
       def destroy
@@ -17,20 +17,27 @@ module Api
       private
 
         def comments_params
-          params.fetch(:comment, {}).permit(:spot_id, :content)
+          params.fetch(:comment, {}).permit(:spot_id, :content, :image)
         end
 
-        def convert_to_json_comment
-          Comment.joins(:user)
-                 .select('
-                    comments.id,
-                    comments.content,
-                    spot_id,
-                    user_id,
-                    comments.created_at,
-                    comments.updated_at,
-                    users.name AS user_name')
-                 .last
+        def convert_to_json_comment(comment)
+          data = Comment.joins(:user)
+                        .select('
+                          comments.id,
+                          comments.content,
+                          spot_id,
+                          user_id,
+                          comments.created_at,
+                          comments.updated_at,
+                          users.name AS user_name')
+                        .last
+
+          url = rails_blob_url(comment.image) if comment.image.attached?
+
+          {
+            comment: data,
+            image: url
+          }
         end
     end
   end
