fbbfad46b66a6d8eec15f7d50931d2c6376db5cb
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-10-24 07:49:22 +0900

    improve: noimage機能を追加

--- frontend/src/components/Spot/SpotShowDialogImageSlide.vue
+++ frontend/src/components/Spot/SpotShowDialogImageSlide.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card flat outlined class="mb-2 py-2">
     <v-slide-group show-arrows>
-      <v-slide-item>
+      <v-slide-item v-if="!photos">
         <v-card class="d-flex mx-2" flat outlined tile width="100px">
           <v-img
             aspect-ratio="1"
@@ -9,6 +9,7 @@
           ></v-img>
         </v-card>
       </v-slide-item>
+
       <v-slide-item v-for="(photo, id) in photos" :key="id">
         <spot-show-dialog-image-slide-dialog :photo="photo" />
       </v-slide-item>
@@ -26,6 +27,12 @@ export default {
 
   components: {
     SpotShowDialogImageSlideDialog
+  },
+
+  data() {
+    return {
+      picture: null
+    }
   },
 
   computed: {
