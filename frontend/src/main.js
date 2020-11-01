c38499efb1ffb6e8950d5be12f080005025767be
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-11-01 10:26:28 +0900

    improve: Vuex/VueRouterの両インスタンスを同期
    StoreActionsでrouter.pushを使用するため

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
+import { sync } from 'vuex-router-sync'
 import vuetify from '@/plugins/vuetify'
 import vee from '@/plugins/vee-validate'
 import 'vuetify/dist/vuetify.min.css'
 import '@/plugins/maps.js'
 
 Vue.config.productionTip = process.env.NODE_ENV === 'production' ? false : true
+
+sync(store, router)
 
 new Vue({
   el: '#app',
