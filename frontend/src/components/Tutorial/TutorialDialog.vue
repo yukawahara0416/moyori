005ae278ccd06f9e6139a66ffe6e334a8e8673c6
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-11-03 11:58:27 +0900

    add: SFC TutorialDialogを新規作成

---
+++ frontend/src/components/Tutorial/TutorialDialog.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-dialog v-model="dialog" width="600">
+    <template v-slot:activator="{ on }">
+      <v-btn v-on="on" @click.stop="placeDetail(spot)">
+        <v-icon class="mx-2">mdi-information-outline</v-icon>
+        詳細をみる
+      </v-btn>
+    </template>
+
+    <tutorial-dialog-carousel />
+  </v-dialog>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import TutorialDialogCarousel from '@/components/Tutorial/TutorialDialogCarousel.vue'
+
+export default {
+  components: {
+    TutorialDialogCarousel
+  },
+
+  computed: {
+    ...mapGetters(['tutorialState']),
+
+    dialog: {
+      get() {
+        return this.tutorialState
+      },
+      set() {
+        this.$store.commit('closeTutorial')
+      }
+    }
+  }
+}
+</script>
