80f1f16b144dad5e001f081089ecb8b087f6f2aa
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-11-04 15:15:26 +0900

    improve: リファクタリング if文のネストを浅く改善

--- frontend/src/components/Buttons/LikeButton.vue
+++ frontend/src/components/Buttons/LikeButton.vue
@@ -30,19 +30,12 @@ export default {
     },
 
     ownLike() {
-      // if (this.isLoggingIn == false) return []
-      // if (this.isLoggingIn == true) {
-      //   return this.spot.likes.filter(like => {
-      //     return like.user_id == this.currentUser.data.id
-      //   })
-      // }
-      if (this.isLoggingIn) {
-        return this.spot.likes.filter(like => {
-          return like.user_id == this.currentUser.data.id
-        })
-      } else {
-        return []
-      }
+      if (this.spot.likes.length == 0) return []
+      if (this.isLoggingIn == false) return []
+
+      return this.spot.likes.filter(like => {
+        return like.user_id == this.currentUser.data.id
+      })
     }
   },
 
@@ -57,68 +50,28 @@ export default {
         this.spot.data,
         'id'
       )
-      // if (this.isLoggingIn == false) {
-      //   this.dialogOn()
-      //   this.pushSnackbar({ message: 'ログインしてください', color: 'error' })
-      //   return
-      // }
-
-      // if (this.isLoggingIn == true && isPosted == false) {
-      //   const result = await this.saveSpot({ spot: spot })
-      //   await this.like({ spot: result, type: type })
-      //   return
-      // }
-
-      // if (
-      //   this.isLoggingIn == true &&
-      //   isPosted == true &&
-      //   this.isLiked == false
-      // ) {
-      //   await this.like({ spot: spot, type: type })
-      //   return
-      // }
-
-      // if (
-      //   this.isLoggingIn == true &&
-      //   isPosted == true &&
-      //   this.isLiked == true
-      // ) {
-      //   await this.unlike({ spot: spot, like: this.ownLike[0], type: type })
-      //   return
-      // }
-
-      // if (this.isLoggingIn == true) {
-      //   if (isPosted == false) {
-      //     const result = await this.saveSpot({ spot: spot })
-      //     await this.like({ spot: result, type: type })
-      //     return
-      //   }
-
-      //   if (isPosted == true && this.isLiked == false) {
-      //     await this.like({ spot: spot, type: type })
-      //     return
-      //   }
-
-      //   if (isPosted == true && this.isLiked == true) {
-      //     await this.unlike({ spot: spot, like: this.ownLike[0], type: type })
-      //     return
-      //   }
-      // }
-
-      if (this.isLoggingIn) {
-        if (isPosted) {
-          if (this.isLiked) {
-            await this.unlike({ spot: spot, like: this.ownLike[0], type: type })
-          } else {
-            await this.like({ spot: spot, type: type })
-          }
-        } else {
-          const result = await this.saveSpot({ spot: spot })
-          await this.like({ spot: result, type: type })
-        }
-      } else {
+      if (this.isLoggingIn == false) {
         this.dialogOn()
         this.pushSnackbar({ message: 'ログインしてください', color: 'error' })
+        return
+      }
+
+      if (this.isLoggingIn == true) {
+        if (isPosted == false) {
+          const result = await this.saveSpot({ spot: spot })
+          await this.like({ spot: result, type: type })
+          return
+        }
+
+        if (isPosted == true && this.isLiked == false) {
+          await this.like({ spot: spot, type: type })
+          return
+        }
+
+        if (isPosted == true && this.isLiked == true) {
+          await this.unlike({ spot: spot, like: this.ownLike[0], type: type })
+          return
+        }
       }
     },
 
