5df17d9898768938668101c109574154017db608
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-11-04 20:53:20 +0900

    improve: リファクタリング if文のネストを浅く改善

--- frontend/src/components/Buttons/PowerWithoutButton.vue
+++ frontend/src/components/Buttons/PowerWithoutButton.vue
@@ -40,23 +40,21 @@ export default {
     },
 
     powerWithsByCurrentUser() {
-      if (this.isLoggingIn) {
-        return this.spot.power_withs.filter(power_with => {
-          return power_with.user_id == this.currentUser.data.id
-        })
-      } else {
-        return []
-      }
+      if (this.spot.power_withs.length == 0) return []
+      if (this.isLoggingIn == false) return []
+
+      return this.spot.power_withs.filter(power_with => {
+        return power_with.user_id == this.currentUser.data.id
+      })
     },
 
     powerWithoutsByCurrentUser() {
-      if (this.isLoggingIn) {
-        return this.spot.power_withouts.filter(power_without => {
-          return power_without.user_id == this.currentUser.data.id
-        })
-      } else {
-        return []
-      }
+      if (this.spot.power_withouts.length == 0) return []
+      if (this.isLoggingIn == false) return []
+
+      return this.spot.power_withouts.filter(power_without => {
+        return power_without.user_id == this.currentUser.data.id
+      })
     }
   },
 
@@ -73,31 +71,40 @@ export default {
       const spot = this.spot
       const type = this.type
 
-      if (this.isLoggingIn) {
-        if (isPosted) {
-          if (this.isPowerWithouting) {
-            await this.unPowerWithout({
-              spot: spot,
-              power_without: this.powerWithoutsByCurrentUser[0],
-              type: type
-            })
-          } else {
-            if (this.isPowerWithing) {
-              await this.unPowerWith({
-                spot: spot,
-                power_with: this.powerWithsByCurrentUser[0],
-                type: type
-              })
-            }
-            await this.powerWithout({ spot: spot, type: type })
-          }
-        } else {
-          const result = await this.saveSpot({ spot: spot })
-          await this.powerWithout({ spot: result, type: type })
-        }
-      } else {
+      if (this.isLoggingIn == false) {
         this.dialogOn()
         this.pushSnackbar({ message: 'ログインしてください', color: 'error' })
+        return
+      }
+
+      if (this.isPostedSpot == false) {
+        const result = await this.saveSpot({ spot: spot })
+        await this.powerWithout({ spot: result, type: type })
+        return
+      }
+
+      if (this.isPowerWithing == true) {
+        await this.unPowerWith({
+          spot: spot,
+          power_with: this.powerWithsByCurrentUser[0],
+          type: type
+        })
+        await this.powerWithout({ spot: spot, type: type })
+        return
+      }
+
+      if (this.isPowerWithouting == false) {
+        await this.powerWithout({ spot: result, type: type })
+        return
+      }
+
+      if (this.isPowerWithouting == true) {
+        await this.unPowerWithout({
+          spot: spot,
+          power_without: this.powerWithoutsByCurrentUser[0],
+          type: type
+        })
+        return
       }
     },
 
