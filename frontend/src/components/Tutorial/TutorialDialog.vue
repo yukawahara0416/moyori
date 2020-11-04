4e9523e31c9997b30c5d3351af92d8da8e744b6c
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-11-04 21:01:38 +0900

    improve: 不要なボタンを削除

--- frontend/src/components/Tutorial/TutorialDialog.vue
+++ frontend/src/components/Tutorial/TutorialDialog.vue
@@ -1,12 +1,5 @@
 <template>
   <v-dialog v-model="dialog" width="600">
-    <template v-slot:activator="{ on }">
-      <v-btn v-on="on" @click.stop="placeDetail(spot)">
-        <v-icon class="mx-2">mdi-information-outline</v-icon>
-        詳細をみる
-      </v-btn>
-    </template>
-
     <tutorial-dialog-carousel />
   </v-dialog>
 </template>
