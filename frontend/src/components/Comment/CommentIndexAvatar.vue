3cc40e8f8150634814351955832e576e423581fd
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-11-06 15:04:44 +0900

    add: コメント投稿者のavatar用単一コンポーネントを新規作成

---
+++ frontend/src/components/Comment/CommentIndexAvatar.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-btn icon>
+    <comment-index-avatar-image :comment="comment" />
+  </v-btn>
+</template>
+
+<script>
+import CommentIndexAvatarImage from '@/components/Comment/CommentIndexAvatarImage.vue'
+
+export default {
+  props: {
+    comment: Object
+  },
+
+  components: {
+    CommentIndexAvatarImage
+  }
+}
+</script>
---
+++ frontend/src/components/Comment/CommentIndexAvatarImage.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-avatar class="white--text headline" color="success" size="24">
+    <v-img v-if="comment.avatar" :src="comment.avatar" alt="プロフィール画像" />
+    <span v-else class="white--text headline">
+      {{ comment.data.user_name.slice(0, 1) }}
+    </span>
+  </v-avatar>
+</template>
+
+<script>
+export default {
+  props: {
+    comment: Object
+  }
+}
+</script>
