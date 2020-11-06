2fe71941c4ab65be10e03799a010ddf0818dcd4b
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-11-06 15:06:10 +0900

    add: コメント画像をダイヤログ表示する単一コンポーネントを新規作成

---
+++ frontend/src/components/Comment/CommentIndexImageDialog.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card class="pa-2">
+    <v-img align="center" :src="comment.image" />
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    comment: Object
+  }
+}
+</script>
