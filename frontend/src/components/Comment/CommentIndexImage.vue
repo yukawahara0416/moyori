06e63b3b339ef6a9bda33de847b497e45219cbee
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-11-06 15:05:54 +0900

    add: コメント画像を表示する単一コンポーネントを新規作成

---
+++ frontend/src/components/Comment/CommentIndexImage.vue
@@ -0,0 +1,35 @@
+<template>
+  <span>
+    <v-img alt="スポット写真" :src="comment.image" @click="dialog = true">
+      <template v-slot:placeholder>
+        <v-row class="fill-height ma-0" align="center" justify="center">
+          <v-progress-circular indeterminate color="success lighten-5" />
+        </v-row>
+      </template>
+    </v-img>
+
+    <v-dialog v-model="dialog" width="600">
+      <comment-index-image-dialog :comment="comment" />
+    </v-dialog>
+  </span>
+</template>
+
+<script>
+import CommentIndexImageDialog from '@/components/Comment/CommentIndexImageDialog.vue'
+
+export default {
+  props: {
+    comment: Object
+  },
+
+  components: {
+    CommentIndexImageDialog
+  },
+
+  data() {
+    return {
+      dialog: false
+    }
+  }
+}
+</script>
