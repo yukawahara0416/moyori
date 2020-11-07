31db8f33b401a9fdc41c1dedf779588ec1680318
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-11-07 20:50:20 +0900

    improve: userモデル バリデーション変更に伴いfactoryも変更

--- backend/spec/factories/users.rb
+++ backend/spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    name             { Faker::Name.name }
-    sequence(:email) { |n| "#{n}_" + Faker::Internet.email }
-    password         { Faker::Internet.password(min_length: 8) }
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 8) }
   end
 end
