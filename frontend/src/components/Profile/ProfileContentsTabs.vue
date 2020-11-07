a8890961ea75ccfec2798099ab00e94f43f8c44e
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-11-07 11:34:24 +0900

    improve: リファクタリング v-tabタブ間を改行

--- frontend/src/components/Profile/ProfileContentsTabs.vue
+++ frontend/src/components/Profile/ProfileContentsTabs.vue
@@ -3,15 +3,19 @@
     <v-tab class="primary--text" href="#posts">
       <v-icon>mdi-map-marker-outline</v-icon>
     </v-tab>
+
     <v-tab class="primary--text" href="#wifi_withs">
       <v-icon>mdi-wifi</v-icon>
     </v-tab>
+
     <v-tab class="primary--text" href="#power_withs">
       <v-icon>mdi-power-plug</v-icon>
     </v-tab>
+
     <v-tab class="primary--text" href="#comments">
       <v-icon>mdi-message-outline</v-icon>
     </v-tab>
+
     <v-tab class="primary--text" href="#likes">
       <v-icon>mdi-heart-outline</v-icon>
     </v-tab>
