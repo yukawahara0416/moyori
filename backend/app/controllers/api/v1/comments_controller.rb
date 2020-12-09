c893367a56e06d685ecf6a937ad35da8eceb49c1
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-09 18:55:24 +0900

    improve: comment/responseのJSON構造を変更ネストを浅くしてフロントエンド側が参照しやすいように

--- backend/app/controllers/api/v1/comments_controller.rb
+++ backend/app/controllers/api/v1/comments_controller.rb
@@ -25,12 +25,20 @@ module Api
                         .select('users.name AS user_name')
                         .last
 
-          url = rails_blob_url(comment.image) if comment.image.attached?
+          user = User.find(comment.user_id)
+          image = rails_blob_url(comment.image) if comment.image.attached?
           avatar = rails_blob_url(user.avatar) if user.avatar.attached?
 
           {
-            data: data,
-            image: url
+            id: comment.id,
+            spot_id: comment.spot_id,
+            user_id: comment.user_id,
+            user_name: data.user_name,
+            avatar: avatar,
+            content: comment.content,
+            image: image,
+            created_at: comment.created_at,
+            updated_at: comment.updated_at
           }
         end
     end
