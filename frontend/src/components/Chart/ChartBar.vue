d1cd6f38e4ee9c8629c250e9302ab570a993b29d
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-09 20:27:08 +0900

    add: 棒グラフ用のマウントファイルを新規作成

---
+++ frontend/src/components/Chart/ChartBar.vue
@@ -0,0 +1,15 @@
+<script>
+import { HorizontalBar, mixins } from 'vue-chartjs'
+
+export default {
+  extends: HorizontalBar,
+  mixins: [mixins.reactiveProp],
+  props: {
+    options: Object
+  },
+
+  mounted() {
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
