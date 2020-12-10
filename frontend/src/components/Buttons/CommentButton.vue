70af4fd91a1e4a106f7a33384a398f7e142a2ab5
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-10 12:54:09 +0900

    move: ボタン関連SFCを移動
    共有していることを明示するため

---
+++ frontend/src/components/Buttons/CommentButton.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <v-btn
+      fab
+      small
+      elevation="1"
+      color="white"
+      @click.stop="openDialog()"
+      @mouseover="mouseover()"
+      @mouseleave="mouseleave()"
+    >
+      <v-icon v-show="isCommenting" color="success">mdi-message</v-icon>
+      <v-icon v-show="!isCommenting" :color="color">{{ icon }}</v-icon>
+      <counter :spot="spot" :target="'comments'" />
+    </v-btn>
+
+    <v-dialog v-model="dialog" width="600">
+      <v-card>
+        <spot-detail-comment-panel :spot="spot" />
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import SpotDetailCommentPanel from '@/components/Spot/SpotDetailCommentPanel.vue'
+import Counter from '@/components/Buttons/Counter.vue'
+
+export default {
+  props: {
+    spot: Object
+  },
+
+  components: {
+    Counter,
+    SpotDetailCommentPanel
+  },
+
+  data() {
+    return {
+      icon: 'mdi-message-outline',
+      color: null,
+      dialog: false
+    }
+  },
+
+  computed: {
+    ...mapGetters(['currentUser', 'isLoggingIn']),
+
+    isCommenting() {
+      return this.yourComments.length > 0 ? true : false
+    },
+
+    yourComments() {
+      return this.spot.hasYourVote('comments', this.currentUser.data.id)
+    }
+  },
+
+  methods: {
+    openDialog() {
+      this.dialog = true
+    },
+
+    mouseover() {
+      this.icon = 'mdi-message'
+      this.color = 'success'
+    },
+
+    mouseleave() {
+      this.icon = 'mdi-message-outline'
+      this.color = null
+    }
+  }
+}
+</script>
+
+<style scoped>
+button::before {
+  background-color: white;
+}
+</style>
