5bc41be71a4e924529078b2e030b6664a71790cd
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-11 14:30:58 +0900

    fix: 不要になったspecをコメントアウト

--- frontend/tests/unit/store/modules/spot/map.spec.js
+++ frontend/tests/unit/store/modules/spot/map.spec.js
@@ -1,82 +1,60 @@
 import { createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import { cloneDeep } from 'lodash'
+// import { axiosBase } from '@/plugins/axios.js'
+// import MockAdapter from 'axios-mock-adapter'
 import map from '@/store/modules/spot/map.js'
-import spot from '@/store/modules/spot/spot.js'
-import { axiosBase } from '@/plugins/axios.js'
-import MockAdapter from 'axios-mock-adapter'
-import queryString from 'query-string'
+// import auth from '@/store/modules/user/auth.js'
+// import queryString from 'query-string'
+
+// const axiosMock = new MockAdapter(axiosBase)
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
-const axiosMock = new MockAdapter(axiosBase)
-
 let store
-let spotStore
-let mockStore
+// let authStore
+// let authMockStore
 
 beforeEach(() => {
   store = new Vuex.Store(cloneDeep(map))
-  spotStore = new Vuex.Store(cloneDeep(spot))
-  mockStore = {
-    namespaced: true,
-    state: {
-      spots: () => []
-    },
+  // authStore = new Vuex.Store(cloneDeep(auth))
 
-    mutations: {
-      addSpotsStore: ({}, arg) => spotStore.commit('addSpotsStore', arg), // eslint-disable-line no-empty-pattern
-      updateDataSpotsStore: ({}, arg) =>
-        spotStore.commit('updateDataSpotsStore', arg) // eslint-disable-line no-empty-pattern
-    }
-  }
-  store.registerModule('spot', cloneDeep(mockStore))
-})
-
-describe('actions', () => {
-  it('spot/addSpotsStore', async () => {
-    var result = { marker: { name: 'test' } }
-    await store.dispatch('addSpotsStore', result)
-    expect(spotStore.state.spots[0]).toBe(result)
-  })
-
-  // it('saveSpot', async () => {
-  //   spotStore.replaceState({
-  //     spots: [{ marker: { place_id: 'test' }, data: { place_id: '' } }]
-  //   })
-
-  //   const params = { spot: { place_id: 'test' } }
-  //   const response = {
-  //     marker: { place_id: 'test' },
-  //     data: { place_id: 'test' }
+  // authMockStore = {
+  //   state: {
+  //     headers: {
+  //       'access-token': 'test',
+  //       'client': 'test',
+  //       'content-type': 'test',
+  //       'uid': 'test'
+  //     }
   //   }
-  //   axiosMock.onPost('/api/v1/spots/save', params).reply(200, response)
-  //   axiosBase.post('/api/v1/spots/save', params).then(res => {
-  //     store.dispatch('updateDataSpotsStore', {
-  //       spot: res.data,
-  //       id: 0,
-  //       prop: 'data'
-  //     })
-  //     expect(spotStore.state.spots[0]).toStrictEqual(response)
-  //   })
-  // })
-
-  it('collateSpot', async () => {
-    const spot = { marker: { place_id: 'test' } }
-    const query = queryString.stringify({ place_id: spot.marker.place_id })
-    const response = {
-      marker: spot.marker.place_id,
-      data: spot.marker.place_id
-    }
-
-    axiosMock.onGet('/api/v1/spots/collate?' + query).reply(200, response)
-
-    const collated = await store.dispatch('collateSpot', spot)
-    expect(collated).toEqual(response)
-  })
-
-  // it('nearbySearch', () => {})
-  // it('textSearch', () => {})
-  // it('geolocate', () => {})
+  // }
+  // store.registerModule('auth', cloneDeep(authMockStore))
 })
+
+// describe('actions', () => {
+//   const spot = { marker: { place_id: 'test' } }
+//   const response = { state: '200 success', data: { id: 1 } }
+
+//   it('saveSpot', () => {
+//     axiosMock
+//       .onPost('/api/v1/spots/save', {
+//         spot: { place_id: spot.marker.place_id }
+//       })
+//       .reply(200, response)
+
+//     return store.dispatch('saveSpot', spot).then(res => {
+//       expect(res.state).toEqual(response.state)
+//     })
+//   })
+
+//   it('collateSpot', async () => {
+//     const query = queryString.stringify({ place_id: spot.marker.place_id })
+//     axiosMock.onGet('/api/v1/spots/collate?' + query).reply(200, response)
+
+//     return store.dispatch('collateSpot', spot).then(res => {
+//       expect(res.state).toEqual(response.state)
+//     })
+//   })
+// })
