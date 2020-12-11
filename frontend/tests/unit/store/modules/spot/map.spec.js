fea9b295f67ae3264d8c84e68ca3e595b12fba3c
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-11 14:46:10 +0900

    improve: 不要なspecを削除

--- frontend/tests/unit/store/modules/spot/map.spec.js
+++ frontend/tests/unit/store/modules/spot/map.spec.js
@@ -1,40 +1,18 @@
 import { createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
-import { cloneDeep } from 'lodash'
-// import { axiosBase } from '@/plugins/axios.js'
-// import MockAdapter from 'axios-mock-adapter'
 import map from '@/store/modules/spot/map.js'
-// import auth from '@/store/modules/user/auth.js'
-// import queryString from 'query-string'
-
-// const axiosMock = new MockAdapter(axiosBase)
+import { cloneDeep } from 'lodash'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
 let store
-// let authStore
-// let authMockStore
 
 beforeEach(() => {
   store = new Vuex.Store(cloneDeep(map))
-  // authStore = new Vuex.Store(cloneDeep(auth))
-
-  // authMockStore = {
-  //   state: {
-  //     headers: {
-  //       'access-token': 'test',
-  //       'client': 'test',
-  //       'content-type': 'test',
-  //       'uid': 'test'
-  //     }
-  //   }
-  // }
-  // store.registerModule('auth', cloneDeep(authMockStore))
 })
 
 const mapObject = { obj1: 'obj1' }
-
 const googleObject = { obj2: 'obj2' }
 
 describe('getters', () => {
@@ -59,29 +37,3 @@ describe('mutations', () => {
     expect(store.state.google).toStrictEqual(googleObject)
   })
 })
-
-// describe('actions', () => {
-//   const spot = { marker: { place_id: 'test' } }
-//   const response = { state: '200 success', data: { id: 1 } }
-
-//   it('saveSpot', () => {
-//     axiosMock
-//       .onPost('/api/v1/spots/save', {
-//         spot: { place_id: spot.marker.place_id }
-//       })
-//       .reply(200, response)
-
-//     return store.dispatch('saveSpot', spot).then(res => {
-//       expect(res.state).toEqual(response.state)
-//     })
-//   })
-
-//   it('collateSpot', async () => {
-//     const query = queryString.stringify({ place_id: spot.marker.place_id })
-//     axiosMock.onGet('/api/v1/spots/collate?' + query).reply(200, response)
-
-//     return store.dispatch('collateSpot', spot).then(res => {
-//       expect(res.state).toEqual(response.state)
-//     })
-//   })
-// })
