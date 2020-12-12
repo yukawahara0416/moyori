a3a98c51bb45fa8252446aa6ff25d0ac995139a5
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-12 09:38:53 +0900

    add: getters関連specを追加

--- frontend/tests/unit/store/modules/spot/form.spec.js
+++ frontend/tests/unit/store/modules/spot/form.spec.js
@@ -12,6 +12,18 @@ beforeEach(() => {
   store = new Vuex.Store(cloneDeep(form))
 })
 
+const data = {
+  place_id: 'test',
+  name: 'test',
+  address: 'test',
+  image: 'test',
+  picture: 'test',
+  phone: 'test',
+  lat: 'test',
+  lng: 'test',
+  url: 'test'
+}
+
 const init = {
   place_id: '',
   name: '',
@@ -24,4 +36,22 @@ const init = {
   url: null
 }
 
-const data = { test: 'test' }
+describe('getters', () => {
+  it('form', () => {
+    store.replaceState({ form: data })
+    expect(store.getters['form']).toEqual(data)
+  })
+
+  it('formData', () => {
+    store.replaceState({ form: data })
+
+    const formData = new FormData()
+    const keys = Object.keys(store.state.form)
+    for (let i = 0; i < keys.length; i++) {
+      if (store.state.form[keys[i]] !== null)
+        formData.append(`spot[${keys[i]}]`, store.state.form[keys[i]])
+    }
+
+    expect(store.getters['formData']).toMatchObject(formData)
+  })
+})
