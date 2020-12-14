cb52af21f434922f9a0bc9160371564b0a9f2db1
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-14 16:26:40 +0900

    add: sessionsを名前解決

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations',
+        sessions: 'api/v1/auth/sessions',
         omniauth_callbacks: 'api/v1/auth/omniauth_callbacks'
       }
       resources :users, only: [:show]
