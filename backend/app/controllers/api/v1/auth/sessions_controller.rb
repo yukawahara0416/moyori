e6bf3aa1c63dec0de0ea3673fe6de02855175648
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-14 16:29:13 +0900

    add: avatarプロパティを追加する処理をoverride

--- backend/app/controllers/api/v1/auth/sessions_controller.rb
+++ backend/app/controllers/api/v1/auth/sessions_controller.rb
@@ -3,8 +3,27 @@ module Api
     module Auth
       class SessionsController < DeviseTokenAuth::SessionsController
         def render_create_success
-          render json: @resource
+          render json: convert_to_sessions(@resource)
         end
+
+        private
+
+          def convert_to_sessions(resource)
+            {
+              data: {
+                id: resource.id,
+                email: resource.email,
+                provider: resource.provider,
+                name: resource.name,
+                avatar: nil,
+                uid: resource.uid,
+                allow_password_change: resource.allow_password_change,
+                created_at: resource.created_at,
+                updated_at: resource.updated_at,
+                credentials: resource.credentials
+              }
+            }
+          end
       end
     end
   end
