51d90e13d8192c9897e135440758c25ce3fe331d
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-15 15:53:52 +0900

    rename: signUpFormDataをsignUpFormにリネーム

--- frontend/src/components/Sign/SignContainerTabItemsSignUpEmail.vue
+++ frontend/src/components/Sign/SignContainerTabItemsSignUpEmail.vue
@@ -18,7 +18,7 @@
                 name="signin"
                 prepend-icon="mdi-account-circle-outline"
                 type="text"
-                v-model="signUpFormData.name"
+                v-model="signUpForm.name"
                 :clearable="true"
                 :error-messages="errors"
                 :success="valid"
@@ -35,7 +35,7 @@
                 name="signin"
                 prepend-icon="mdi-email-outline"
                 type="text"
-                v-model="signUpFormData.email"
+                v-model="signUpForm.email"
                 :clearable="true"
                 :error-messages="errors"
                 :success="valid"
@@ -52,7 +52,7 @@
                 name="password"
                 prepend-icon="mdi-lock-outline"
                 type="password"
-                v-model="signUpFormData.password"
+                v-model="signUpForm.password"
                 @keyup.enter="signUp()"
                 :clearable="true"
                 :error-messages="errors"
@@ -98,7 +98,7 @@ import { mapGetters, mapMutations, mapActions } from 'vuex'
 
 export default {
   computed: {
-    ...mapGetters(['signUpFormData'])
+    ...mapGetters(['signUpForm'])
   },
 
   methods: {
@@ -106,7 +106,7 @@ export default {
       'setCurrentUser',
       'setHeaders',
       'clearSignInFormData',
-      'clearSignUpFormData',
+      'clearSignUpForm',
       'changeSignTab'
     ]),
     ...mapActions([
@@ -118,7 +118,7 @@ export default {
 
     signUpHanlder: async function() {
       try {
-        const response = await this.signUp(this.signUpFormData)
+        const response = await this.signUp(this.signUpForm)
         const currentUser = response.data.data
         const headers = response.headers
 
@@ -127,7 +127,7 @@ export default {
 
         this.dialogOff('dialogSign')
         this.clearSignInFormData()
-        this.clearSignUpFormData()
+        this.clearSignUpForm()
         this.pushSnackbarSuccess({
           message: 'アカウントを登録しました。MoYoRiへようこそ！'
         })
