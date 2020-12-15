92f699312f04cf1060561f6cd798ba27eaf20169
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-15 16:00:07 +0900

    rename: signInFormDataをsignInFormにリネーム

--- frontend/src/components/Sign/SignContainerTabItemsSignInEmail.vue
+++ frontend/src/components/Sign/SignContainerTabItemsSignInEmail.vue
@@ -18,7 +18,7 @@
                 name="login"
                 prepend-icon="mdi-email-outline"
                 type="text"
-                v-model="signInFormData.email"
+                v-model="signInForm.email"
                 :clearable="true"
                 :error-messages="errors"
                 :success="valid"
@@ -36,7 +36,7 @@
                 name="password"
                 prepend-icon="mdi-lock-outline"
                 type="password"
-                v-model="signInFormData.password"
+                v-model="signInForm.password"
                 @keyup.enter="signInHandler()"
                 :clearable="true"
                 :error-messages="errors"
@@ -82,7 +82,7 @@ import { mapGetters, mapMutations, mapActions } from 'vuex'
 
 export default {
   computed: {
-    ...mapGetters(['isLoggingIn', 'signInFormData'])
+    ...mapGetters(['isLoggingIn', 'signInForm'])
   },
 
   methods: {
@@ -90,7 +90,7 @@ export default {
       'setHeaders',
       'setCurrentUser',
       'clearSignUpForm',
-      'clearSignInFormData',
+      'clearSignInForm',
       'changeSignTab'
     ]),
     ...mapActions([
@@ -107,7 +107,7 @@ export default {
           throw new Error('すでにログイン中です')
         }
 
-        const response = await this.signIn(this.signInFormData)
+        const response = await this.signIn(this.signInForm)
 
         const currentUser = response.data.data
         const headers = response.headers
@@ -116,7 +116,7 @@ export default {
         await this.setHeaders(headers)
 
         this.dialogOff('dialogSign')
-        this.clearSignInFormData()
+        this.clearSignInForm()
         this.clearSignUpForm()
         this.pushSnackbarSuccess({ message: 'ログインしました' })
       } catch (error) {
