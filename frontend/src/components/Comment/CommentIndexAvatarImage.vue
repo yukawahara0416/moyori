3b9cfccf382bd106a03c817ba9b287df5a2ce897
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-17 09:41:12 +0900

    add: router-linkを追加

--- frontend/src/components/Comment/CommentIndexAvatarImage.vue
+++ frontend/src/components/Comment/CommentIndexAvatarImage.vue
@@ -1,10 +1,16 @@
 <template>
-  <v-avatar class="white--text headline" color="success" size="24">
-    <v-img v-if="comment.avatar" :src="comment.avatar" alt="プロフィール画像" />
-    <span v-else class="white--text headline">
-      {{ comment.user_name.slice(0, 1) }}
-    </span>
-  </v-avatar>
+  <router-link :to="{ name: 'profile', params: { id: comment.user_id } }">
+    <v-avatar class="white--text headline" color="success" size="24">
+      <v-img
+        v-if="comment.avatar"
+        :src="comment.avatar"
+        alt="プロフィール画像"
+      />
+      <span v-else class="white--text headline">
+        {{ comment.user_name.slice(0, 1) }}
+      </span>
+    </v-avatar>
+  </router-link>
 </template>
 
 <script>
