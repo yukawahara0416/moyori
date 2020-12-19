cd62661cd9328af05b9be38371dd3286ae2f1f84
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-19 14:31:57 +0900

    add: specの実施項目をリストアップ

---
+++ frontend/tests/unit/views/Profile.spec.js
@@ -0,0 +1,14 @@
+// props id
+// created this.fetchData
+// getter user
+// getter currentUser
+
+// mutations
+// lodingOn
+// lodingOff
+// clearSpotsStore
+// clearUserStore
+// setUserStore
+
+// actions
+// getUser
