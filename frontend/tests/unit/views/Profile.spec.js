a1f19a8feef287267d69d9cbd5ec683e7740addb
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-19 15:32:26 +0900

    add: 基本のimportとbeforeEachを定義

--- frontend/tests/unit/views/Profile.spec.js
+++ frontend/tests/unit/views/Profile.spec.js
@@ -12,3 +12,42 @@
 
 // actions
 // getUser
+
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import Component from '@/views/Profile.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+// let wrapper
+let store
+let spot
+let user
+
+beforeEach(() => {
+  spot = {
+    namespaced: true,
+    mutations: {}
+  }
+
+  user = {
+    namespaced: true,
+    mutations: {}
+  }
+
+  store = new Vuex.Store({
+    modules: {
+      spot,
+      user
+    }
+  })
+
+  // wrapper = shallowMount(Component, {
+  //   localVue,
+  //   store,
+  //   methods: {
+  //     fetchData
+  //   }
+  // })
+})
