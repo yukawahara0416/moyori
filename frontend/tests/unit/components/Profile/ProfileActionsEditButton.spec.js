64149a6321eadcfba1950cbf409130bee13d0262
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-20 10:15:43 +0900

    remove: 不要なコメントアウトを削除

--- frontend/tests/unit/components/Profile/ProfileActionsEditButton.spec.js
+++ frontend/tests/unit/components/Profile/ProfileActionsEditButton.spec.js
@@ -1,69 +1,22 @@
 import { mount, createLocalVue } from '@vue/test-utils'
 import Component from '@/components/Profile/ProfileActionsEditButton.vue'
-// import Vuex from 'vuex'
-// import Vuetify from 'vuetify'
 
 const localVue = createLocalVue()
-// localVue.use(Vuex)
-// localVue.use(Vuetify)
 
 let wrapper
 
 const openDialog = jest.fn()
 const closeDialog = jest.fn()
 
-// let store
-// let vuetify
-// let getters
-// let actions
-
 beforeEach(() => {
-  // getters = {
-  //   dialogProfileEdit: () => true
-  // }
-
-  // actions = {
-  //   dialogOn: jest.fn(),
-  //   dialogOff: jest.fn()
-  // }
-
-  // store = new Vuex.Store({
-  //   getters,
-  //   actions
-  // })
-
-  // vuetify = new Vuetify()
-
   wrapper = mount(Component, {
     localVue,
     methods: { openDialog, closeDialog }
-    // store
-    // vuetify
   })
 })
 
-// afterEach(() => {
-//   wrapper.destroy()
-// })
-
-// describe('getters', () => {
-//   it('dialogProfileEdit', () => {
-//     expect(wrapper.vm.dialogProfileEdit).toEqual(getters.dialogProfileEdit())
-//   })
-// })
-
-// describe('computed', () => {
-//   it('dialog', () => {})
-// })
-
 describe('v-on', () => {
-  // const app = document.createElement('div')
-  // app.setAttribute('data-app', true)
-  // document.body.append(app)
-
   it('dialogOn', () => {
-    // const event = jest.fn()
-    // wrapper.setMethods({ dialogOn: event })
     wrapper.find('.v-btn').trigger('click')
     expect(openDialog).toHaveBeenCalledTimes(1)
   })
@@ -73,13 +26,6 @@ describe('v-on', () => {
     expect(wrapper.emitted().closeDialog).toBeTruthy()
   })
 })
-
-// describe('actions', () => {
-//   it('dialogOn', () => {
-//     wrapper.vm.dialogOn()
-//     expect(actions.dialogOn).toHaveBeenCalled()
-//   })
-// })
 
 describe('template', () => {
   it('snapshot', () => {
