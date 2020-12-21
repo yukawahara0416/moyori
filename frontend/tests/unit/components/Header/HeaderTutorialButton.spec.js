5fffa4c8480ff30869c9442417afde4dcef59673
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-21 19:53:04 +0900

    add: 主要変数を宣言

--- frontend/tests/unit/components/Header/HeaderTutorialButton.spec.js
+++ frontend/tests/unit/components/Header/HeaderTutorialButton.spec.js
@@ -1,3 +1,10 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import Component from '@/components/Header/Header.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+let wrapper
+let store
+let dialog
