bb6697b0af97dd05b655611032ae02b3080b1f89
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-23 12:35:30 +0900

    rename: specファイル名を変更

--- frontend/tests/unit/components/Header/HeaderDrawerButton.spec.js
+++ frontend/tests/unit/components/Header/HeaderDrawerButton.spec.js
@@ -1,5 +1,5 @@
 import { shallowMount } from '@vue/test-utils'
-import Component from '@/components/Header/HeaderDrawer.vue'
+import Component from '@/components/Header/HeaderDrawerButton.vue'
 
 let wrapper
 let propsData
