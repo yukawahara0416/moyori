9c0b2c5e629c479140e47c76f4055e0c6b2b7d5e
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-23 16:01:47 +0900

    add: wrapperにVuetifyを注入

--- frontend/tests/unit/components/Card/CardFrameContent.spec.js
+++ frontend/tests/unit/components/Card/CardFrameContent.spec.js
@@ -1,19 +1,25 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuetify from 'vuetify'
 import Component from '@/components/Card/CardFrameContent.vue'
 
 const localVue = createLocalVue()
+localVue.use(Vuetify)
 
 let wrapper
 let propsData
+let vuetify
 
 beforeEach(() => {
   propsData = {
     spot: { data: { id: 1, name: 'test' } }
   }
 
+  vuetify = new Vuetify()
+
   wrapper = shallowMount(Component, {
     localVue,
-    propsData
+    propsData,
+    vuetify
   })
 })
 
