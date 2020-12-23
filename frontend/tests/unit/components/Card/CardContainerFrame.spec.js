1d9204b66dbc78b49dcaf8472291aad8b0978452
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-23 15:21:01 +0900

    remove: 不要なprops/typeを削除

--- frontend/tests/unit/components/Card/CardContainerFrame.spec.js
+++ frontend/tests/unit/components/Card/CardContainerFrame.spec.js
@@ -56,14 +56,15 @@ describe('props', () => {
     expect(wrapper.props().spot).toStrictEqual(propsData.spot)
     expect(wrapper.props().spot instanceof Object).toBe(true)
   })
+
   it('id', () => {
     expect(wrapper.props().id).toStrictEqual(propsData.id)
     expect(typeof wrapper.vm.$props.id).toBe('number')
   })
-  it('type', () => {
-    expect(wrapper.props().type).toStrictEqual(propsData.type)
-    expect(typeof wrapper.vm.$props.type).toBe('string')
-  })
+  // it('type', () => {
+  //   expect(wrapper.props().type).toStrictEqual(propsData.type)
+  //   expect(typeof wrapper.vm.$props.type).toBe('string')
+  // })
 })
 
 describe('v-on', () => {
