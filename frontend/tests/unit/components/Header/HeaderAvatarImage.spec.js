373f82e284a485fcfb9b96a9d829f63746df4e73
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-23 10:24:10 +0900

    remove: vuex関連コードを削除
    vueファイルで使用しておらず不要だったため

--- frontend/tests/unit/components/Header/HeaderAvatarImage.spec.js
+++ frontend/tests/unit/components/Header/HeaderAvatarImage.spec.js
@@ -1,13 +1,10 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
-import Vuex from 'vuex'
 import Component from '@/components/Header/HeaderAvatarImage.vue'
 
 const localVue = createLocalVue()
-localVue.use(Vuex)
 
 let wrapper
 let propsData
-let store
 
 beforeEach(() => {
   propsData = {
@@ -22,8 +19,7 @@ beforeEach(() => {
 
   wrapper = shallowMount(Component, {
     localVue,
-    propsData,
-    store
+    propsData
   })
 })
 
