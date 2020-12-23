c57235a78fff200f04763dd41e408729de54baf4
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-23 16:12:47 +0900

    rename: ファイル名を変更

---
+++ frontend/tests/unit/components/Card/CardFrameContentAction.spec.js
@@ -0,0 +1,38 @@
+import { shallowMount } from '@vue/test-utils'
+import Component from '@/components/Card/CardFrameContentAction.vue'
+
+let wrapper
+let propsData
+
+beforeEach(() => {
+  propsData = {
+    spot: { marker: { name: 'test' }, data: { id: 1 } },
+    id: 1,
+    type: 'map'
+  }
+
+  wrapper = shallowMount(Component, {
+    propsData
+  })
+})
+
+afterEach(() => {
+  wrapper.destroy()
+})
+
+describe('proprs', () => {
+  it('spot', () => {
+    expect(wrapper.props().spot).toStrictEqual(propsData.spot)
+    expect(wrapper.props().spot instanceof Object).toBe(true)
+  })
+  it('type', () => {
+    expect(wrapper.props().type).toStrictEqual(propsData.type)
+    expect(typeof wrapper.vm.$props.type).toBe('string')
+  })
+})
+
+describe('template', () => {
+  it('snapshot', () => {
+    expect(wrapper.vm.$el).toMatchSnapshot()
+  })
+})
