271016b35f9b64f55d258378ca02e8ce1ec6c4e7
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-23 16:40:55 +0900

    remove: afterEachを削除

--- frontend/tests/unit/components/Card/CardFrameContentTitle.spec.js
+++ frontend/tests/unit/components/Card/CardFrameContentTitle.spec.js
@@ -17,10 +17,6 @@ beforeEach(() => {
   })
 })
 
-afterEach(() => {
-  wrapper.destroy()
-})
-
 describe('props', () => {
   it('spot', () => {
     expect(wrapper.props().spot).toStrictEqual(propsData.spot)
