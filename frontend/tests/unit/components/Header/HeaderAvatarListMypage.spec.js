35baebc18cccad5b5dc989b5b4a2bb96e492d650
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-23 11:38:15 +0900

    add: createLocalVueをimport

--- frontend/tests/unit/components/Header/HeaderAvatarListMypage.spec.js
+++ frontend/tests/unit/components/Header/HeaderAvatarListMypage.spec.js
@@ -1,5 +1,7 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Component from '@/components/Header/HeaderAvatarListMypage.vue'
+
+const localVue = createLocalVue()
 
 let wrapper
 let propsData
@@ -10,6 +12,7 @@ beforeEach(() => {
   }
 
   wrapper = shallowMount(Component, {
+    localVue,
     propsData
   })
 })
