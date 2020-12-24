266361b59d2d1c75fbf63f2c8904e1040250398f
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-25 07:21:14 +0900

    add: vueインスタンスを生成

--- frontend/tests/unit/components/Search/SearchFilterSwitch.spec.js
+++ frontend/tests/unit/components/Search/SearchFilterSwitch.spec.js
@@ -2,3 +2,11 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Component from '@/components/Search/SearchFilterSwitch.vue'
 
 const localVue = createLocalVue()
+
+let wrapper
+
+beforeEach(() => {
+  wrapper = shallowMount(Component, {
+    localVue
+  })
+})
