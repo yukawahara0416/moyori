b7b783fdc4641ac2c06fb3bc54d5dd3ef835b1e3
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-24 20:23:38 +0900

    add: vuexインスタンスにmodules/spotを注入

--- frontend/tests/unit/components/Search/SearchFilterRadius.spec.js
+++ frontend/tests/unit/components/Search/SearchFilterRadius.spec.js
@@ -6,10 +6,31 @@ const localVue = createLocalVue()
 localVue.use(Vuex)
 
 let wrapper
+let store
+let spot
 
 beforeEach(() => {
+  spot = {
+    namespaced: true,
+    getters: {
+      radius: () => {
+        return { name: '500m', value: 500 }
+      }
+    },
+    mutations: {
+      setRadius: jest.fn()
+    }
+  }
+
+  store = new Vuex.Store({
+    modules: {
+      spot
+    }
+  })
+
   wrapper = shallowMount(Component, {
-    localVue
+    localVue,
+    store
   })
 })
 
