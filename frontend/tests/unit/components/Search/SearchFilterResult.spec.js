7c7fc811c1199151e7915e7de422761d69f548b4
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-24 20:56:20 +0900

    add: spec追加 template/v-card-subtitle text

---
+++ frontend/tests/unit/components/Search/SearchFilterResult.spec.js
@@ -0,0 +1,31 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Component from '@/components/Search/SearchFilterResult.vue'
+
+const localVue = createLocalVue()
+
+let wrapper
+let propsData
+
+beforeEach(() => {
+  propsData = {
+    spots: [{ data: { id: 1 } }, { data: { id: 2 } }],
+    filteredSpots: [{ data: { id: 2 } }]
+  }
+
+  wrapper = shallowMount(Component, {
+    localVue,
+    propsData
+  })
+})
+
+describe('template', () => {
+  it('v-card-subtitle text', () => {
+    expect(wrapper.find('v-card-subtitle-stub').text()).toEqual(
+      '2 件中 1 件を表示'
+    )
+  })
+
+  it('snapshot', () => {
+    expect(wrapper.vm.$el).toMatchSnapshot()
+  })
+})
