a300a0d26fbbd69f2c961a800a9e070f54450526
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-24 20:12:07 +0900

    add: localVueを追加

--- frontend/tests/unit/components/Search/SearchFilterRadius.spec.js
+++ frontend/tests/unit/components/Search/SearchFilterRadius.spec.js
@@ -1,3 +1,6 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import Component from '@/components/Search/SearchFilterRadius.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
