d6425ee7973419376fd7a9f1b335d67dd12ba21d
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-25 08:52:56 +0900

    add: 主要moduleをimport

---
+++ frontend/tests/unit/components/Search/SearchFilterType.spec.js
@@ -0,0 +1,10 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import Component from '@/components/Search/SearchFilterType.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+let wrapper
+let store
+let spot
