0211329fa8c084907e530b6d7f82143a7451affb
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-26 07:37:09 +0900

    add: localVueを生成

--- frontend/tests/unit/components/Sign/SignContainerTabItemsSignUpOauth.spec.js
+++ frontend/tests/unit/components/Sign/SignContainerTabItemsSignUpOauth.spec.js
@@ -1,3 +1,5 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import Component from '@/components/Sign/SignContainerTabItemsSignUpOauth.vue'
+
+const localVue = createLocalVue()
