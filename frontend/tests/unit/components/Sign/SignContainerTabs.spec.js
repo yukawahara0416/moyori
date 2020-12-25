d199981fc78ca952c97841d5c10071fda7174702
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-26 07:54:05 +0900

    remove: 不要なspecを削除

--- frontend/tests/unit/components/Sign/SignContainerTabs.spec.js
+++ frontend/tests/unit/components/Sign/SignContainerTabs.spec.js
@@ -2,25 +2,10 @@ import { shallowMount } from '@vue/test-utils'
 import Component from '@/components/Sign/SignContainerTabs.vue'
 
 let wrapper
-let propsData
 
 beforeEach(() => {
-  propsData = {
-    tabs: 'tab-1'
-  }
-
   wrapper = shallowMount(Component, {
     propsData
-  })
-})
-
-afterEach(() => {
-  wrapper.destroy()
-})
-
-describe('props', () => {
-  it('tabs', () => {
-    expect(wrapper.props().tabs).toEqual(propsData.tabs)
   })
 })
 
