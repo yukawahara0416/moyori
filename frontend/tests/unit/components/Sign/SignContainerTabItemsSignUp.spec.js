0d440c851fc7d3482379aeebbc48e1a485e4477c
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-26 07:00:49 +0900

    remove: 不要なspecを削除

--- frontend/tests/unit/components/Sign/SignContainerTabItemsSignUp.spec.js
+++ frontend/tests/unit/components/Sign/SignContainerTabItemsSignUp.spec.js
@@ -1,73 +1,13 @@
-import { mount, createLocalVue } from '@vue/test-utils'
-import Vuex from 'vuex'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Component from '@/components/Sign/SignContainerTabItemsSignUp.vue'
 
 const localVue = createLocalVue()
-localVue.use(Vuex)
 
 let wrapper
-let getters
-let actions
-let store
 
 beforeEach(() => {
-  getters = {
-    signUpFormData: () => ({
-      name: 'tester',
-      email: 'tester@example.com',
-      password: 'password',
-      password_confirmation: 'password'
-    }),
-    currentUser: () => ({ data: { id: 1 } })
-  }
-
-  actions = {
-    signUp: jest.fn()
-  }
-
-  store = new Vuex.Store({
-    getters,
-    actions
-  })
-
-  wrapper = mount(Component, {
-    localVue,
-    store,
-    stubs: ['policy']
-  })
-})
-
-afterEach(() => {
-  wrapper.destroy()
-})
-
-describe('getters', () => {
-  it('signUpFormData', () => {
-    expect(wrapper.vm.signUpFormData).toEqual(getters.signUpFormData())
-  })
-  it('currentUser', () => {
-    expect(wrapper.vm.currentUser).toEqual(getters.currentUser())
-  })
-})
-
-describe('v-on', () => {
-  it('signUp', () => {
-    const event = jest.fn()
-    wrapper.setMethods({ signUp: event })
-    wrapper.find('.v-btn').trigger('click')
-    expect(event).toHaveBeenCalledTimes(1)
-  })
-})
-
-describe('actions', () => {
-  it('signUp', () => {
-    wrapper.vm.signUp()
-    expect(actions.signUp).toHaveBeenCalled()
-    // expect(actions.signUp).toHaveBeenCalledWith(
-    //   expect.any(Object),
-    //   getters.signUpFormData,
-    //   undefined
-    // )
+  wrapper = shallowMount(Component, {
+    localVue
   })
 })
 
