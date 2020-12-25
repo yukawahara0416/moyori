61a08edec07a3dc8b881494d334bfcf584c6dfbc
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-26 08:03:32 +0900

    add: wrapperにVuexインスタンスを注入

--- frontend/tests/unit/components/Sign/SignContainerTabs.spec.js
+++ frontend/tests/unit/components/Sign/SignContainerTabs.spec.js
@@ -1,11 +1,33 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
 import Component from '@/components/Sign/SignContainerTabs.vue'
 
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
 let wrapper
+let store
+let tab
 
 beforeEach(() => {
+  tab = {
+    getters: {
+      signTab: () => 'signup'
+    },
+    mutations: {
+      changeSignTab: jest.fn()
+    }
+  }
+
+  store = new Vuex.Store({
+    modules: {
+      tab
+    }
+  })
+
   wrapper = shallowMount(Component, {
-    propsData
+    localVue,
+    store
   })
 })
 
