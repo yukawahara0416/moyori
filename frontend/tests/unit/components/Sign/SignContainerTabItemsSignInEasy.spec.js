122830b4f567c7e166a21f16e27d0356a654e9d9
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-25 19:52:42 +0900

    add: 主要moduleをimport

---
+++ frontend/tests/unit/components/Sign/SignContainerTabItemsSignInEasy.spec.js
@@ -0,0 +1,6 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import Component from '@/components/Sign/SignContainerTabItemsSignInEasy.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
