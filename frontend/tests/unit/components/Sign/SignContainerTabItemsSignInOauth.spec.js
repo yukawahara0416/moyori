12e9add5bf63dbaaa2499688c17f81f945bd1074
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-26 06:24:36 +0900

    add: localVueを生成

--- frontend/tests/unit/components/Sign/SignContainerTabItemsSignInOauth.spec.js
+++ frontend/tests/unit/components/Sign/SignContainerTabItemsSignInOauth.spec.js
@@ -1,3 +1,5 @@
 import { mount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import Component from '@/components/Sign/SignContainerTabItemsSignInOauth.vue'
+
+const localVue = createLocalVue()
