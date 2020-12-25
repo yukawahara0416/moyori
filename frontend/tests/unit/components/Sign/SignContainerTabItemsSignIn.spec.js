e83dc5289005afcbd063d42ff54ff5bcdd168cdd
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-25 19:50:51 +0900

    remove: 不要なspecを削除

--- frontend/tests/unit/components/Sign/SignContainerTabItemsSignIn.spec.js
+++ frontend/tests/unit/components/Sign/SignContainerTabItemsSignIn.spec.js
@@ -12,68 +12,6 @@ beforeEach(() => {
   })
 })
 
-describe('getters', () => {
-  it('signInFormData', () => {
-    expect(wrapper.vm.signInFormData).toEqual(getters.signInFormData())
-  })
-  it('currentUser', () => {
-    expect(wrapper.vm.currentUser).toEqual(getters.currentUser())
-  })
-  it('headers', () => {
-    expect(wrapper.vm.headers).toEqual(getters.headers())
-  })
-})
-
-describe('v-on', () => {
-  it('signInAsTestUser', () => {
-    const event = jest.fn()
-    wrapper.setMethods({ signInAsTestUser: event })
-    wrapper
-      .findAll('.v-btn')
-      .at(0)
-      .trigger('click')
-    expect(event).toHaveBeenCalledTimes(1)
-  })
-  it('signInByTwitter', () => {
-    const event = jest.fn()
-    wrapper.setMethods({ signIn: event })
-    wrapper
-      .findAll('.v-btn')
-      .at(1)
-      .trigger('click')
-    expect(event).toHaveBeenCalledTimes(1)
-  })
-  it('signIn', () => {
-    const event = jest.fn()
-    wrapper.setMethods({ signIn: event })
-    wrapper
-      .findAll('.v-btn')
-      .at(2)
-      .trigger('click')
-    expect(event).toHaveBeenCalledTimes(1)
-  })
-})
-
-describe('actions', () => {
-  it('signIn', () => {
-    wrapper.vm.signIn()
-    expect(actions.signIn).toHaveBeenCalled()
-  })
-  // it('signInByTwitter', () => {
-  //   wrapper.vm.signIn()
-  //   expect(actions.signIn).toHaveBeenCalled()
-  // })
-  it('signInAsTestUser', () => {
-    wrapper.vm.signInAsTestUser()
-    expect(actions.signIn).toHaveBeenCalled()
-    // expect(actions.signIn).toHaveBeenCalledWith(
-    //   expect.any(Object),
-    //   getters.signInFormData,
-    //   undefined
-    // )
-  })
-})
-
 describe('template', () => {
   it('snapshot', () => {
     expect(wrapper.vm.$el).toMatchSnapshot()
