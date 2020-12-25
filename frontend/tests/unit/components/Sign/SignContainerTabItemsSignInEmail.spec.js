4377f49beff6be60fbf1b521078446d022c9490c
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-25 20:18:17 +0900

    add: 主要moduleをimport

---
+++ frontend/tests/unit/components/Sign/SignContainerTabItemsSignInEmail.spec.js
@@ -0,0 +1,10 @@
+import { mount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import Component from '@/components/Sign/SignContainerTabItemsSignInEasy.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+let wrapper
+let store
+let auth
