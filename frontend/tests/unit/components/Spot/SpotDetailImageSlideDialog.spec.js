bc7f5f8a729633bf787b4b4ecb5d64d780aa96a5
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-27 08:06:21 +0900

    remove: コメントアウトを削除

--- frontend/tests/unit/components/Spot/SpotDetailImageSlideDialog.spec.js
+++ frontend/tests/unit/components/Spot/SpotDetailImageSlideDialog.spec.js
@@ -1,9 +1,3 @@
-// props photo
-// v - on openDialog
-// methods openDialog
-// template :srd
-// template v-model dialog
-
 import { mount, createLocalVue } from '@vue/test-utils'
 import Component from '@/components/Spot/SpotDetailImageSlideDialog.vue'
 
