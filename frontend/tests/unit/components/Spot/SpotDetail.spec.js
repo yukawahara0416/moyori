20918125fe91f60347cbfbd89c43477d97a8b398
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-26 11:16:38 +0900

    add: 必要なspecを洗い出し

---
+++ frontend/tests/unit/components/Spot/SpotDetail.spec.js
@@ -0,0 +1,11 @@
+// props spot
+// v-on
+// emit
+
+//template
+// spot-detail-title spot
+// spot-detail-image-slide spot
+// spot-detail-wifi-panel spot
+// spot-detail-power-panel spot
+// spot-detail-comment-panel spot
+// spot-detail-info-panel spot
