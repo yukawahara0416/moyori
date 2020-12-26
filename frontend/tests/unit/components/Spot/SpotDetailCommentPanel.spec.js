8bd92a18174f230e0e74fc2cb28dcfb7feac7e75
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-26 12:03:43 +0900

    add: spec追加 props/spot

--- frontend/tests/unit/components/Spot/SpotDetailCommentPanel.spec.js
+++ frontend/tests/unit/components/Spot/SpotDetailCommentPanel.spec.js
@@ -3,19 +3,30 @@
 // template spot
 
 import { shallowMount, createLocalVue } from '@vue/test-utils'
-import Component from '@/components/Search/SearchMapContainer.vue'
+import Component from '@/components/Spot/SpotDetailCommentPanel.vue'
 
 const localVue = createLocalVue()
 
 let wrapper
+let propsData
 
 beforeEach(() => {
+  propsData = {
+    spot: { data: { id: 1 }, comments: [{ data: { id: 1 } }] }
+  }
+
   wrapper = shallowMount(Component, {
-    localVue
+    localVue,
+    propsData
   })
 })
 
-describe('props', () => {})
+describe('props', () => {
+  it('spot', () => {
+    expect(wrapper.props().spot).toStrictEqual(propsData.spot)
+    expect(wrapper.props().spot instanceof Object).toBe(true)
+  })
+})
 
 describe('template', () => {
   it('snapshot', () => {
