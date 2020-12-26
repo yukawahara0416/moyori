4bee9900412119eab150b85e97fd3d2f01a08389
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-26 16:07:06 +0900

    add: 主要moduleをimport

---
+++ frontend/tests/unit/components/Spot/SpotDetailImageSlide.spec.js
@@ -0,0 +1,7 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Component from '@/components/Spot/SpotDetailImageSlide.vue'
+
+const localVue = createLocalVue()
+
+let wrapper
+let propsData
