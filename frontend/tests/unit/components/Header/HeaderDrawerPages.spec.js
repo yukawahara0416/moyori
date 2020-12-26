91e76019947110af4d79db1048731efcc1a5b698
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-26 09:29:44 +0900

    add: spec追加 template/v-list-item has :to

--- frontend/tests/unit/components/Header/HeaderDrawerPages.spec.js
+++ frontend/tests/unit/components/Header/HeaderDrawerPages.spec.js
@@ -12,6 +12,12 @@ beforeEach(() => {
 })
 
 describe('template', () => {
+  it('v-list-item has :to', () => {
+    const target = wrapper.findAll('v-list-item-stub')
+    expect(target.at(0).attributes().to).toEqual('/about')
+    expect(target.at(1).attributes().to).toEqual('/rules')
+    expect(target.at(2).attributes().to).toEqual('/privacy')
+  })
   it('snapshot', () => {
     expect(wrapper.vm.$el).toMatchSnapshot()
   })
