b5ff453adf5e6f8ca2e581dba355455b41de05dd
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-27 15:38:22 +0900

    add: spec computed/analyzeVote return good

--- frontend/tests/unit/components/Spot/SpotDetailWifiPanel.spec.js
+++ frontend/tests/unit/components/Spot/SpotDetailWifiPanel.spec.js
@@ -176,4 +176,48 @@ describe('computed', () => {
     expect(wrapper.vm.$el).toMatchSnapshot()
   })
 
+  it('analyzeVote return good', () => {
+    propsData = {
+      spot: {
+        data: { id: 1 },
+        wifi_withs: [
+          { id: 2, created_at: '2020-12-01T00:00:00.000Z' },
+          { id: 3, created_at: '2020-12-01T00:00:00.000Z' }
+        ],
+        wifi_withouts: [{ id: 4, created_at: '2020-12-31T00:00:00.000Z' }]
+      }
+    }
+
+    wrapper = shallowMount(Component, {
+      localVue,
+      propsData
+    })
+
+    expect(wrapper.vm.analyzeVote).toEqual('good')
+    expect(wrapper.find('v-icon-stub').text()).toEqual('mdi-circle-outline')
+    expect(wrapper.find('strong').text()).toEqual('そこそこ期待できます^_^')
+    expect(wrapper.vm.$el).toMatchSnapshot()
+  })
+  it('analyzeVote return good', () => {
+    propsData = {
+      spot: {
+        data: { id: 1 },
+        wifi_withs: [
+          { id: 2, created_at: '2020-12-01T00:00:00.000Z' },
+          { id: 3, created_at: '2020-12-01T00:00:00.000Z' }
+        ],
+        wifi_withouts: [{ id: 4, created_at: '2020-12-31T00:00:00.000Z' }]
+      }
+    }
+
+    wrapper = shallowMount(Component, {
+      localVue,
+      propsData
+    })
+
+    expect(wrapper.vm.analyzeVote).toEqual('good')
+    expect(wrapper.find('v-icon-stub').text()).toEqual('mdi-circle-outline')
+    expect(wrapper.find('strong').text()).toEqual('そこそこ期待できます^_^')
+    expect(wrapper.vm.$el).toMatchSnapshot()
+  })
 })
