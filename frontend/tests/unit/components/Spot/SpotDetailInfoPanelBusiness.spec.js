6680f3685516f7600ee876e9f70ed79a22faddb5
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-27 11:28:00 +0900

    add: propsData assign spot

--- frontend/tests/unit/components/Spot/SpotDetailInfoPanelBusiness.spec.js
+++ frontend/tests/unit/components/Spot/SpotDetailInfoPanelBusiness.spec.js
@@ -6,3 +6,21 @@ const localVue = createLocalVue()
 let wrapper
 let propsData
 
+  propsData = {
+    spot: {
+      data: {
+        id: 1,
+        opening_hours: {
+          weekday_text: [
+            '月曜日: 10時00分～19時30分',
+            '火曜日: 10時00分～19時30分',
+            '水曜日: 10時00分～19時30分',
+            '木曜日: 10時00分～19時30分',
+            '金曜日: 10時00分～19時30分',
+            '土曜日: 10時00分～19時30分',
+            '日曜日: 10時00分～19時30分'
+          ]
+        }
+      }
+    }
+  }
