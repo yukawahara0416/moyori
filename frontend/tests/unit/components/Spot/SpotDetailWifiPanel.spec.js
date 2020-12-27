a9822885d0c0ad56b60f62ac41b1926ebf2b2481
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-27 15:35:41 +0900

    add: spec computed/latestVote return withsNewer

--- frontend/tests/unit/components/Spot/SpotDetailWifiPanel.spec.js
+++ frontend/tests/unit/components/Spot/SpotDetailWifiPanel.spec.js
@@ -97,4 +97,20 @@ describe('computed', () => {
     expect(wrapper.vm.countVote).toEqual('withoutsMany')
   })
 
+  it('latestVote return withsNewer', () => {
+    propsData = {
+      spot: {
+        data: { id: 1 },
+        wifi_withs: [{ id: 2, created_at: '2020-12-31T00:00:00.000Z' }],
+        wifi_withouts: [{ id: 3, created_at: '2020-12-01T00:00:00.000Z' }]
+      }
+    }
+
+    wrapper = shallowMount(Component, {
+      localVue,
+      propsData
+    })
+
+    expect(wrapper.vm.latestVote).toEqual('withsNewer')
+  })
 })
