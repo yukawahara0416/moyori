65b437a38b09e8aa097f2e8b8936caba9a592f15
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-27 10:35:22 +0900

    add: spec candidata

---
+++ frontend/tests/unit/components/Spot/SpotDetailInfoPanelBusiness.spec.js
@@ -0,0 +1,22 @@
+/**
+ * template
+ * v-if="spot.data.opening_hours"
+ * v-if="readMore"
+ * {{ businessDays.slice(0, maxChar) }}
+ * ...続きをよむ
+ * {{ businessDays }}
+ *
+ * v-on
+ * activateReadMore
+ *
+ * props
+ * spot
+ *
+ * computed
+ * businessDays
+ * isAboveLimit
+ * readMore
+ *
+ * methods
+ * activateReadMore
+ */
