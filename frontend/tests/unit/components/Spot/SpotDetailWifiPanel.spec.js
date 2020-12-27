a7d37745591eb792d5393de0e4204721734e7857
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-27 15:30:55 +0900

    add: propsData assign spot

--- frontend/tests/unit/components/Spot/SpotDetailWifiPanel.spec.js
+++ frontend/tests/unit/components/Spot/SpotDetailWifiPanel.spec.js
@@ -6,3 +6,11 @@ const localVue = createLocalVue()
 let wrapper
 let propsData
 
+  propsData = {
+    spot: {
+      data: { id: 1 },
+      wifi_withs: [{ id: 2 }, { id: 3 }],
+      wifi_withouts: [{ id: 4 }, { id: 5 }]
+    }
+  }
+
