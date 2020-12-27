3c5d26b7aae31c02be1413c539f9786e9c3dbb92
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-27 14:41:34 +0900

    add: propsData assign spot

--- frontend/tests/unit/components/Spot/SpotDetailPowerPanel.spec.js
+++ frontend/tests/unit/components/Spot/SpotDetailPowerPanel.spec.js
@@ -6,3 +6,11 @@ const localVue = createLocalVue()
 let wrapper
 let propsData
 
+  propsData = {
+    spot: {
+      data: { id: 1 },
+      power_withs: [{ id: 2 }, { id: 3 }],
+      power_withouts: [{ id: 4 }, { id: 5 }]
+    }
+  }
+
