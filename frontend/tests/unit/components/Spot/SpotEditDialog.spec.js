13901e579e41a274bb1f4fb6f9cc8242405f1965
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-27 19:59:36 +0900

    add: create localVue

--- frontend/tests/unit/components/Spot/SpotEditDialog.spec.js
+++ frontend/tests/unit/components/Spot/SpotEditDialog.spec.js
@@ -1,3 +1,5 @@
 import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import Component from '@/components/Spot/SpotEditDialog.vue'
+
+const localVue = createLocalVue()
