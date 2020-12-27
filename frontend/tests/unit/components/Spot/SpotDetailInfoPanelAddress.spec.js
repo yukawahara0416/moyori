ab8784c6c8ed33d9e7b47abd726d6d2f5cbdb490
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-27 10:27:51 +0900

    remove: commentout

--- frontend/tests/unit/components/Spot/SpotDetailInfoPanelAddress.spec.js
+++ frontend/tests/unit/components/Spot/SpotDetailInfoPanelAddress.spec.js
@@ -1,13 +1,3 @@
-/**
- * template :href
- * template address
- *
- * props spot
- *
- * computed address
- * computed url
- */
-
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Component from '@/components/Spot/SpotDetailInfoPanelAddress.vue'
 
