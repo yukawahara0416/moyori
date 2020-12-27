79a69f40f87f6ef6fea31d3dd24e95f77bdf53f4
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-27 15:13:39 +0900

    add: declare let on global

--- frontend/tests/unit/components/Spot/SpotDetailToolbar.spec.js
+++ frontend/tests/unit/components/Spot/SpotDetailToolbar.spec.js
@@ -2,3 +2,5 @@ import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import Component from '@/components/Spot/SpotDetailToolbar.vue'
 
 const localVue = createLocalVue()
+
+let wrapper
