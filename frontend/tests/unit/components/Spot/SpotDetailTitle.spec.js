b5eda02f2253cac40ff4151cd309ad6a03fdd35d
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-27 14:59:57 +0900

    add: spec props/spot

--- frontend/tests/unit/components/Spot/SpotDetailTitle.spec.js
+++ frontend/tests/unit/components/Spot/SpotDetailTitle.spec.js
@@ -20,4 +20,8 @@ beforeEach(() => {
 })
 
 describe('props', () => {
+  it('spot', () => {
+    expect(wrapper.props().spot).toStrictEqual(propsData.spot)
+    expect(wrapper.props().spot instanceof Object).toBe(true)
+  })
 })
