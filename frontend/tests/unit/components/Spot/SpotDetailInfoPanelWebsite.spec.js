8c576a2ec86ba15be15d833a2d6502225241e84a
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-27 13:24:13 +0900

    add: declare let on global

--- frontend/tests/unit/components/Spot/SpotDetailInfoPanelWebsite.spec.js
+++ frontend/tests/unit/components/Spot/SpotDetailInfoPanelWebsite.spec.js
@@ -2,3 +2,7 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Component from '@/components/Spot/SpotDetailInfoPanelWebsite.vue'
 
 const localVue = createLocalVue()
+
+let wrapper
+let propsData
+
