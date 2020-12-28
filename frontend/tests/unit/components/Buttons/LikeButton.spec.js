a8d620a62e49e62ff2416fa21ae76381f9ca31da
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-28 13:57:41 +0900

    add: options assign data/likes

--- frontend/tests/unit/components/Buttons/LikeButton.spec.js
+++ frontend/tests/unit/components/Buttons/LikeButton.spec.js
@@ -18,12 +18,13 @@ let map
 let tab
 
 beforeEach(() => {
-  propsData = {
-    spot: {
-      marker: { name: 'test' },
-      data: { id: 1 },
-      likes: [{ data: { id: 1 } }]
-    },
+  options = {
+    data: { id: 1 },
+    likes: [
+      { id: 1, user_id: 1 },
+      { id: 2, user_id: 2 }
+    ]
+  }
     id: 1,
     type: 'map'
   }
