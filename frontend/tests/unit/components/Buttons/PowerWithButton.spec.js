47c95e0ef52b5f79dba0287b107ffd0ec07f08c6
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-28 15:14:24 +0900

    add: spec computed/isPowerWithing is false

--- frontend/tests/unit/components/Buttons/PowerWithButton.spec.js
+++ frontend/tests/unit/components/Buttons/PowerWithButton.spec.js
@@ -125,3 +125,30 @@ describe('comnputed', () => {
     expect(wrapper.vm.isPowerWithing).toBe(true)
   })
 
+  it('isPowerWithing is false', () => {
+    options = {
+      data: { id: 1 },
+      power_withs: [
+        { id: 1, user_id: 2 },
+        { id: 2, user_id: 2 }
+      ],
+      power_withouts: [
+        { id: 3, user_id: 1 },
+        { id: 4, user_id: 2 }
+      ]
+    }
+
+    data = new Spot(options)
+
+    propsData = {
+      spot: data
+    }
+
+    wrapper = shallowMount(Component, {
+      localVue,
+      propsData,
+      store
+    })
+    expect(wrapper.vm.isPowerWithing).toBe(false)
+  })
+
