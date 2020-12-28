dda5814db35ba1da058128bd009091447cfe0565
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-28 13:56:45 +0900

    add: import component

--- frontend/tests/unit/components/Buttons/LikeButton.spec.js
+++ frontend/tests/unit/components/Buttons/LikeButton.spec.js
@@ -1,6 +1,7 @@
 import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import { Spot } from '@/class/Spot.js'
+import Component from '@/components/Buttons/LikeButton.vue'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
