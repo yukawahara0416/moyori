43086d6581453ce1273c062eb198d5f3dcc34057
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-28 13:19:03 +0900

    add: declare let on global

--- frontend/tests/unit/components/Buttons/Counter.spec.js
+++ frontend/tests/unit/components/Buttons/Counter.spec.js
@@ -2,3 +2,7 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Component from '@/components/Buttons/Counter.vue'
 
 const localVue = createLocalVue()
+
+let wrapper
+let propsData
+
