078c13c146402a89cc94651e6cb6cc43cd7f1827
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-28 19:32:36 +0900

    add: create localVue

--- frontend/tests/unit/components/Buttons/WifiWithButton.spec.js
+++ frontend/tests/unit/components/Buttons/WifiWithButton.spec.js
@@ -2,3 +2,5 @@ import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import { Spot } from '@/class/Spot.js'
 import Component from '@/components/Buttons/WifiWithButton.vue'
+
+const localVue = createLocalVue()
