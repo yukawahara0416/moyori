841937460e5659eb93df28922f34e363d112d3d9
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-28 15:09:44 +0900

    add: option assign data/power_withs/power_withouts

--- frontend/tests/unit/components/Buttons/PowerWithButton.spec.js
+++ frontend/tests/unit/components/Buttons/PowerWithButton.spec.js
@@ -18,5 +18,17 @@ let map
 let tab
 
 beforeEach(() => {
+  options = {
+    data: { id: 1 },
+    power_withs: [
+      { id: 1, user_id: 1 },
+      { id: 2, user_id: 2 }
+    ],
+    power_withouts: [
+      { id: 3, user_id: 1 },
+      { id: 4, user_id: 2 }
+    ]
+  }
+
 })
 
