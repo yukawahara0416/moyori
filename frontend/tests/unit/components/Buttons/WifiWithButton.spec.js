311886397fa01df18a8007827750ff304ce51c5e
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-28 19:39:20 +0900

    add: spec computed/isWifiWithouting is false

--- frontend/tests/unit/components/Buttons/WifiWithButton.spec.js
+++ frontend/tests/unit/components/Buttons/WifiWithButton.spec.js
@@ -156,4 +156,31 @@ describe('computed', () => {
     expect(wrapper.vm.isWifiWithouting).toBe(true)
   })
 
+  it('isWifiWithouting is false', () => {
+    options = {
+      data: { id: 1 },
+      wifi_withs: [
+        { id: 1, user_id: 1 },
+        { id: 2, user_id: 2 }
+      ],
+      wifi_withouts: [
+        { id: 3, user_id: 2 },
+        { id: 4, user_id: 2 }
+      ]
+    }
+
+    data = new Spot(options)
+
+    propsData = {
+      spot: data
+    }
+
+    wrapper = shallowMount(Component, {
+      localVue,
+      propsData,
+      store
+    })
+    expect(wrapper.vm.isWifiWithouting).toBe(false)
+  })
+
 })
