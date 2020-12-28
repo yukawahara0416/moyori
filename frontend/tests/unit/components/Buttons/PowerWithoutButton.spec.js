abe2d000a63b89763641ca6297e10447cf97b925
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-28 18:51:50 +0900

    add: spec computed/yourPowerWith

--- frontend/tests/unit/components/Buttons/PowerWithoutButton.spec.js
+++ frontend/tests/unit/components/Buttons/PowerWithoutButton.spec.js
@@ -183,4 +183,8 @@ describe('comnputed', () => {
     expect(wrapper.vm.isPowerWithouting).toBe(false)
   })
 
+  it('yourPowerWith', () => {
+    expect(wrapper.vm.yourPowerWith).toMatchObject([options.power_withs[0]])
+  })
+
 })
