616bdbd9ced87fb04aa22e11b942c73c0b225998
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-28 19:16:11 +0900

    add: create localVue

--- frontend/tests/unit/components/Buttons/SpotDetailShowButton.spec.js
+++ frontend/tests/unit/components/Buttons/SpotDetailShowButton.spec.js
@@ -2,3 +2,5 @@ import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import { Spot } from '@/class/Spot.js'
 import Component from '@/components/Buttons/SpotDetailShowButton.vue'
+
+const localVue = createLocalVue()
