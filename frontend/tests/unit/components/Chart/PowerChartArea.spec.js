cc2419c79177625640f84767d45c3339b3d4433d
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-29 10:29:20 +0900

    add: declare let on global

--- frontend/tests/unit/components/Chart/PowerChartArea.spec.js
+++ frontend/tests/unit/components/Chart/PowerChartArea.spec.js
@@ -2,3 +2,7 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Component from '@/components/Chart/PowerChartArea.vue'
 
 const localVue = createLocalVue()
+
+let wrapper
+let propsData
+
