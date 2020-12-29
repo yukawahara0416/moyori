cfef7bfd9030f542cdb5c6cf61adcd8013bf7ab8
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-29 20:09:16 +0900

    add: spec computed/yourPowerWithout

--- frontend/tests/unit/components/Comment/CommentPostDialogForm.spec.js
+++ frontend/tests/unit/components/Comment/CommentPostDialogForm.spec.js
@@ -274,5 +274,10 @@ describe('computed', () => {
     expect(wrapper.vm.yourPowerWith).toMatchObject([options.power_withs[0]])
   })
 
+  it('yourPowerWithout', () => {
+    expect(wrapper.vm.yourPowerWithout).toMatchObject([
+      options.power_withouts[0]
+    ])
+  })
 })
 })
