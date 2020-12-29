c04567cc0c387a7429dd6469ba0e9ef135bc57b6
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-29 16:29:32 +0900

    add: declare let on global

--- frontend/tests/unit/components/Comment/CommentIndexImageDialog.spec.js
+++ frontend/tests/unit/components/Comment/CommentIndexImageDialog.spec.js
@@ -2,3 +2,6 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Component from '@/components/Comment/CommentIndexImageDialog.vue'
 
 const localVue = createLocalVue()
+
+let wrapper
+let propsData
