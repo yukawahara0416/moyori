d38ee8b9709da1e9b14f7e0c8333fa3e2e439504
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-29 16:52:22 +0900

    add: create localVue

--- frontend/tests/unit/components/Comment/CommentPostDialog.spec.js
+++ frontend/tests/unit/components/Comment/CommentPostDialog.spec.js
@@ -1,3 +1,5 @@
 import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import Component from '@/components/Comment/CommentPostDialog.vue'
+
+const localVue = createLocalVue()
