4f25e2c3caa4c709ab44cb7eaad72bf692529d3a
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-29 20:01:33 +0900

    add: options assign data/wifi_withs/wifi_withouts/power_withs/power_withouts

--- frontend/tests/unit/components/Comment/CommentPostDialogForm.spec.js
+++ frontend/tests/unit/components/Comment/CommentPostDialogForm.spec.js
@@ -18,4 +18,23 @@ let map
 let tab
 
 beforeEach(() => {
+  options = {
+    data: { id: 1 },
+    wifi_withs: [
+      { id: 1, user_id: 1 },
+      { id: 2, user_id: 2 }
+    ],
+    wifi_withouts: [
+      { id: 3, user_id: 1 },
+      { id: 4, user_id: 2 }
+    ],
+    power_withs: [
+      { id: 1, user_id: 1 },
+      { id: 2, user_id: 2 }
+    ],
+    power_withouts: [
+      { id: 3, user_id: 1 },
+      { id: 4, user_id: 2 }
+    ]
+  }
 })
