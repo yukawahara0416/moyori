786d8bcb26757a7a33ef12f1bbbf6325eca6e3d0
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-29 21:47:13 +0900

    add: create localVue

--- frontend/tests/unit/components/Utility/Snackbar.spec.js
+++ frontend/tests/unit/components/Utility/Snackbar.spec.js
@@ -2,3 +2,5 @@ import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import snackStore from '@/store/modules/utility/snackbar.js'
 import Component from '@/components/Utility/Snackbar.vue'
+
+const localVue = createLocalVue()
