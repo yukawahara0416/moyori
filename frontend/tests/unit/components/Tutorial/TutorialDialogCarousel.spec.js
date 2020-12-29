5d8bd5dc0e75bcce4814362144235feb28799c5c
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-29 20:57:09 +0900

    add: import component

--- frontend/tests/unit/components/Tutorial/TutorialDialogCarousel.spec.js
+++ frontend/tests/unit/components/Tutorial/TutorialDialogCarousel.spec.js
@@ -1,3 +1,4 @@
 import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import Vuetify from 'vuetify'
+import Component from '@/components/Tutorial/TutorialDialogCarousel.vue'
