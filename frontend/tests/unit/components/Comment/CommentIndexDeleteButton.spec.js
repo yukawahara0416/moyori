1968722b274ac2b5f70642e6479a572a0adeafbe
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-29 15:02:52 +0900

    add: declare let on global

--- frontend/tests/unit/components/Comment/CommentIndexDeleteButton.spec.js
+++ frontend/tests/unit/components/Comment/CommentIndexDeleteButton.spec.js
@@ -2,3 +2,7 @@ import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import Component from '@/components/Comment/CommentIndexDeleteButton.vue'
 
 const localVue = createLocalVue()
+
+let wrapper
+let propsData
+
