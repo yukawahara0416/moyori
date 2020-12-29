3f6b41ac842ddc7dba3b0c9e9ad62267b5c8c8be
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-29 21:46:51 +0900

    add: import component

--- frontend/tests/unit/components/Utility/Snackbar.spec.js
+++ frontend/tests/unit/components/Utility/Snackbar.spec.js
@@ -1,3 +1,4 @@
 import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import snackStore from '@/store/modules/utility/snackbar.js'
+import Component from '@/components/Utility/Snackbar.vue'
