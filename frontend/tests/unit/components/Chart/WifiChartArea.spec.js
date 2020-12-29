77bbddd9b062fa6769e38e87b1a2d3bcc20ef39e
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-29 10:18:48 +0900

    add: props assign spot/wifi_withs/wifi_withouts

--- frontend/tests/unit/components/Chart/WifiChartArea.spec.js
+++ frontend/tests/unit/components/Chart/WifiChartArea.spec.js
@@ -7,4 +7,17 @@ let wrapper
 let propsData
 
 beforeEach(() => {
+  propsData = {
+    spot: {
+      data: { id: 1 },
+      wifi_withs: [
+        { id: 1, created_at: '2020-12-01T00:00:00.000Z' },
+        { id: 2, created_at: '2020-12-02T00:00:00.000Z' }
+      ],
+      wifi_withouts: [
+        { id: 3, created_at: '2020-12-03T00:00:00.000Z' },
+        { id: 4, created_at: '2020-12-04T00:00:00.000Z' }
+      ]
+    }
+  }
 })
