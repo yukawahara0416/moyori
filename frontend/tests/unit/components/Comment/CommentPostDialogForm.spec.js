6f37695dc7241a745596db95ead10c7cc6dfdef7
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-29 20:07:28 +0900

    add: spec computed/isWifiWithouting is false

--- frontend/tests/unit/components/Comment/CommentPostDialogForm.spec.js
+++ frontend/tests/unit/components/Comment/CommentPostDialogForm.spec.js
@@ -167,7 +167,35 @@ describe('computed', () => {
     expect(wrapper.vm.isWifiWithouting).toBe(true)
   })
 
-  data = new Spot(options)
+  it('isWifiWithouting is false', () => {
+    options = {
+      data: { id: 1 },
+      wifi_withs: [
+        { id: 1, user_id: 1 },
+        { id: 2, user_id: 2 }
+      ],
+      wifi_withouts: [
+        { id: 3, user_id: 2 },
+        { id: 4, user_id: 2 }
+      ]
+    }
+
+    data = new Spot(options)
+
+    propsData = {
+      spot: data
+    }
+
+    wrapper = shallowMount(Component, {
+      localVue,
+      propsData,
+      store,
+      stubs: ['ValidationObserver']
+    })
+
+    expect(wrapper.vm.isWifiWithouting).toBe(false)
+  })
+
   data = new Spot(options)
 })
 })
