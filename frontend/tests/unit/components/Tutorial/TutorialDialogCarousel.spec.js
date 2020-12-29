1b980db3c7706912cbafeba7f273c6552d5b262d
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-29 20:57:25 +0900

    add: create localVue

--- frontend/tests/unit/components/Tutorial/TutorialDialogCarousel.spec.js
+++ frontend/tests/unit/components/Tutorial/TutorialDialogCarousel.spec.js
@@ -2,3 +2,5 @@ import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import Vuetify from 'vuetify'
 import Component from '@/components/Tutorial/TutorialDialogCarousel.vue'
+
+const localVue = createLocalVue()
