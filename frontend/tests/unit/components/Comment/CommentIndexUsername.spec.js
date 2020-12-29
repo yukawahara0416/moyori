16335e5c55d890f49ce83a393f7c6b644cb4ae90
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-29 16:36:33 +0900

    add: declare let on global

--- frontend/tests/unit/components/Comment/CommentIndexUsername.spec.js
+++ frontend/tests/unit/components/Comment/CommentIndexUsername.spec.js
@@ -2,3 +2,6 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Component from '@/components/Comment/CommentIndexUsername.vue'
 
 const localVue = createLocalVue()
+
+let wrapper
+let propsData
