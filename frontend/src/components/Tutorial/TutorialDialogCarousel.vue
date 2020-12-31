a7b778521c251c7b1bd38fb7ef3927e81582e7c0
yukawahara0416 <yu.kawahara0416@gmail.com>
2020-12-31 19:52:52 +0900

    improve: decrement colors

--- frontend/src/components/Tutorial/TutorialDialogCarousel.vue
+++ frontend/src/components/Tutorial/TutorialDialogCarousel.vue
@@ -27,14 +27,7 @@ import { mapMutations } from 'vuex'
 export default {
   data() {
     return {
-      colors: [
-        'indigo',
-        'warning',
-        'pink darken-2',
-        'red lighten-1',
-        'deep-purple accent-4'
-      ],
-      slides: ['First', 'Second', 'Third', 'Fourth', 'Fifth']
+      colors: ['indigo', 'warning', 'pink darken-2', 'red lighten-1'],
     }
   },
 
