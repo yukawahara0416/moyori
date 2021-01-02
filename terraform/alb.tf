5151cd50692e6eae90553ff95feed8ced293dad3
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-01-02 10:48:32 +0900

    add: コメントアウトを解除

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -57,48 +57,48 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-# resource "aws_lb_listener" "https" {
-#   load_balancer_arn = aws_lb.example.arn
-#   port              = "443"
-#   protocol          = "HTTPS"
-#   certificate_arn   = aws_acm_certificate.example.arn
-#   ssl_policy        = "ELBSecurityPolicy-2016-08"
-#   default_action {
-#     type = "forward"
-#     target_group_arn = aws_lb_target_group.example.arn
-#   }
-# }
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.example.arn
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
+}
 
-# resource "aws_lb_listener_rule" "example" {
-#   listener_arn = aws_lb_listener.https.arn
-#   priority     = 100
-#   action {
-#     type = "forward"
-#     target_group_arn = aws_lb_target_group.example.arn
-#   }
-#   condition {
-#     path_pattern {
-#       values = ["/*"]
-#     }
-#   }
-# }
+resource "aws_lb_listener_rule" "example" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 100
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
+}
 
-# resource "aws_lb_target_group" "example" {
-#   name                 = "${var.aws_resource_prefix}-tg"
-#   target_type          = "ip"
-#   vpc_id               = aws_vpc.example.id
-#   port                 = 80
-#   protocol             = "HTTP"
-#   deregistration_delay = 300
-#   health_check {
-#     path                = "/"
-#     healthy_threshold   = 10
-#     unhealthy_threshold = 4
-#     timeout             = 20
-#     interval            = 60
-#     matcher             = 200
-#     port                = "traffic-port"
-#     protocol            = "HTTP"
-#   }
-#   depends_on = [aws_lb.example]
-# }
+resource "aws_lb_target_group" "example" {
+  name                 = "${var.aws_resource_prefix}-tg"
+  target_type          = "ip"
+  vpc_id               = aws_vpc.example.id
+  port                 = 80
+  protocol             = "HTTP"
+  deregistration_delay = 300
+  health_check {
+    path                = "/"
+    healthy_threshold   = 10
+    unhealthy_threshold = 4
+    timeout             = 20
+    interval            = 60
+    matcher             = 200
+    port                = "traffic-port"
+    protocol            = "HTTP"
+  }
+  depends_on = [aws_lb.example]
+}
