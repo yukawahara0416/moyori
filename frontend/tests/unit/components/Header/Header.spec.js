ffbf777e990ef7a8f88d3e783a80d32977347a35
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-01-08 14:23:56 +0900

    add: Vuetifyをimport

--- frontend/tests/unit/components/Header/Header.spec.js
+++ frontend/tests/unit/components/Header/Header.spec.js
@@ -1,5 +1,6 @@
 import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
+import Vuetify from 'vuetify'
 import Component from '@/components/Header/Header.vue'
 
 const localVue = createLocalVue()
