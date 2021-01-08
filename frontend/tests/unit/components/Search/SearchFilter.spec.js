41031a0ebd54998c2aea36f506c27185134e8f1c
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-01-08 14:25:34 +0900

    add: Vuetifyをimport

--- frontend/tests/unit/components/Search/SearchFilter.spec.js
+++ frontend/tests/unit/components/Search/SearchFilter.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
+import Vuetify from 'vuetify'
 import Component from '@/components/Search/SearchFilter.vue'
 
 const localVue = createLocalVue()
