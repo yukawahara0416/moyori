faf080b4e1a0f227a8186acc7ee17cacf81d9a58
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-01-08 21:49:11 +0900

    add: plugins/createPersistedStateにkey:sessionを命名

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -37,7 +37,7 @@ export default new Vuex.Store({
 
   plugins: [
     createPersistedState({
-      paths: ['auth.currentUser', 'auth.headers', 'dialog.dialogTutorial'],
+      key: 'session',
       storage: window.sessionStorage
     })
   ]
