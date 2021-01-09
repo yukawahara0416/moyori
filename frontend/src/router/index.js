48651e9b08ced9d9ca0a77d6cd9a283686e1fca9
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-01-09 16:22:30 +0900

    remove: import Aboutを削除

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '@/views/Home.vue'
 import Search from '@/views/Search.vue'
 import Profile from '@/views/Profile.vue'
 import Rules from '@/views/Rules.vue'
