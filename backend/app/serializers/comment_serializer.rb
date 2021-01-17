0c6132a90b6dc5698c502dcbae23adc1cbfca9fc
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-01-17 10:39:26 +0900

    add: attribute/avatarを追加

--- backend/app/serializers/comment_serializer.rb
+++ backend/app/serializers/comment_serializer.rb
@@ -2,5 +2,6 @@ class CommentSerializer < ActiveModel::Serializer
   attributes :id,
              :spot_id,
              :user_id,
-             :user_name
+             :user_name,
+             :avatar
 end
