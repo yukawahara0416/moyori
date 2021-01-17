e3b285eec2853e5150834ba9b9004e20284d43e3
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-01-17 10:10:01 +0900

    add: 関連モデルLikeを追加

--- backend/app/serializers/spot_serializer.rb
+++ backend/app/serializers/spot_serializer.rb
@@ -24,4 +24,6 @@ class SpotSerializer < ActiveModel::Serializer
   def picture
     object.picture.attachment.service.send(:object_for, object.picture.key).public_url if object.picture.attached?
   end
+
+  has_many :likes
 end
