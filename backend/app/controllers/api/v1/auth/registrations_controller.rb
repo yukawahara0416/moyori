6c402df6223f60b62186146530d7f3a8db2c990f
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-01-22 20:24:04 +0900

    remove: 不要なmethodを削除

--- backend/app/controllers/api/v1/auth/registrations_controller.rb
+++ backend/app/controllers/api/v1/auth/registrations_controller.rb
@@ -32,30 +32,6 @@ module Api
           def account_update_params
             params.permit(:name, :email, :avatar)
           end
-
-          def convert_to_registrations(resource)
-            {
-              data: {
-                id: resource.id,
-                email: resource.email,
-                provider: resource.provider,
-                name: resource.name,
-                avatar: set_avatar(resource.id),
-                uid: resource.uid,
-                allow_password_change: resource.allow_password_change,
-                created_at: resource.created_at,
-                updated_at: resource.updated_at,
-                credentials: resource.credentials
-              }
-            }
-          end
-
-          def set_avatar(id)
-            user = User.find(id)
-            avatar = rails_blob_url(user.avatar) if user.avatar.attached?
-
-            avatar
-          end
       end
     end
   end
