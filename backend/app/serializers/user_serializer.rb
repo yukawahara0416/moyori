be0c6b523e7beffbfc4bdd5ce1f09c8aeb60faf9
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-01-23 10:22:30 +0900

    add: Spotオブジェクトを取得

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -44,6 +44,7 @@ class UserSerializer < ActiveModel::Serializer
   has_many :power_withs do
     spots = []
     object.power_withs.each do |item|
+      spot = Spot.find(item.spot_id)
     end
     spots
 
