459d9d446caafa1d1e5cd19454fa760d1b27bf49
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-01-23 11:34:13 +0900

    add: Vuetifyをimport

--- frontend/tests/unit/components/Buttons/SpotDetailShowButton.spec.js
+++ frontend/tests/unit/components/Buttons/SpotDetailShowButton.spec.js
@@ -1,5 +1,6 @@
 import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
+import Vuetify from 'vuetify'
 import { Spot } from '@/class/Spot.js'
 import Component from '@/components/Buttons/SpotDetailShowButton.vue'
 
