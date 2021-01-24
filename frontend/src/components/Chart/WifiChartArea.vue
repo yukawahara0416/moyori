dad52ae9f84607ed920c977ba384ffce9f69d0f7
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-01-25 06:50:04 +0900

    add: method/sortDataの引数にfirstDayを追加 0点データを追加するため

--- frontend/src/components/Chart/WifiChartArea.vue
+++ frontend/src/components/Chart/WifiChartArea.vue
@@ -132,7 +132,7 @@ export default {
     },
 
     // 時間を昇順で並べ替えます
-    sortData(xyData) {
+    sortData(xyData, firstDay) {
       const arry = [...xyData]
 
       arry.sort(function compare(a, b) {
