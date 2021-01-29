5be43ea8c343750fb75b17d40431700feb2e1ec5
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-01-29 21:41:44 +0900

    add: mutations/clearSpotFormDataにプロパティphoto_referenceを追加

--- frontend/src/store/modules/spot/form.js
+++ frontend/src/store/modules/spot/form.js
@@ -59,7 +59,7 @@ export default {
         place_id: '',
         address: '',
         name: '',
-        image: null,
+        photo_reference: null,
         picture: null,
         phone: null,
         lat: '',
