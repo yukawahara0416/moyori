dbfc1a23998a409d9d122145819bf4c94870c179
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-02-01 06:34:59 +0900

    add: vote/unVoteにisMyPageを渡す

--- frontend/src/components/Buttons/PowerWithButton.vue
+++ frontend/src/components/Buttons/PowerWithButton.vue
@@ -118,7 +118,8 @@ export default {
             target,
             tab,
             headers,
-            route
+            route,
+            isMyPage
           })
         }
 
@@ -131,7 +132,8 @@ export default {
             target,
             tab,
             headers,
-            route
+            route,
+            isMyPage
           })
           this.pushSnackbarSuccess({
             message: '「電源あるよ」を取り消しました'
@@ -146,7 +148,8 @@ export default {
           params,
           tab,
           headers,
-          route
+          route,
+          isMyPage,
         })
         this.pushSnackbarSuccess({ message: '「電源あるよ」を投票しました' })
       } catch (error) {
