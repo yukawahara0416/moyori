73759634ba8ee15a72f58d4987956ff3d6893072
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-01-31 11:37:31 +0900

    add: comment.imageのactive storage URLを取得するメソッドを追加

--- backend/app/models/comment.rb
+++ backend/app/models/comment.rb
@@ -5,4 +5,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :spot
   validates :content, presence: true, length: { maximum: 1000 }
+
+  def image_url
+    image.attached? ? url_for(image) : nil
+  end
 end
