904f311c3479d7801aa8583ef343021b9650a0ea
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-02-08 07:06:15 +0900

    add: templateを追加

---
+++ frontend/src/components/Spot/SpotDeleteButtonDialog.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card>
+    <v-card-title class="headline">
+      スポットを削除しますか？
+    </v-card-title>
+    <v-card-text>この操作は取り消すことができません。</v-card-text>
+
+    <v-card-actions>
+      <v-spacer />
+
+      <v-btn color="green darken-1" text>
+        キャンセル
+      </v-btn>
+
+      <v-btn color="error darken-1" text>
+        削除する
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    spot: Object
+  },
+
+}
+</script>
