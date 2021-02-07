6e70fd79e34ae75c4abb6aae74695482a3b6da49
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-02-08 07:01:21 +0900

    add: templateを作成

---
+++ frontend/src/components/Spot/SpotDeleteButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <span>
+    <v-btn small class="mb-3" color="error" type="submit" text>
+      <v-icon>mdi-eraser</v-icon>
+      スポットを削除する
+    </v-btn>
+
+    <v-dialog v-model="dialog" width="600">
+      <spot-delete-button-dialog :spot="spot" />
+    </v-dialog>
+  </span>
+</template>
+
+<script>
+import SpotDeleteButtonDialog from '@/components/Spot/SpotDeleteButtonDialog.vue'
+
+export default {
+  props: {
+    spot: Object
+  },
+
+  components: {
+    SpotDeleteButtonDialog
+  },
+}
+</script>
