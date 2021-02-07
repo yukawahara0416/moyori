5aec86f79ced7d378219e5c086a390d8efb26be4
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-02-08 07:14:30 +0900

    add: then処理を追加　spot_idのみを返す

--- frontend/src/components/Spot/SpotDeleteButtonDialog.vue
+++ frontend/src/components/Spot/SpotDeleteButtonDialog.vue
@@ -30,6 +30,9 @@ export default {
     deleteSpot(id, headers) {
       return axiosBase
         .delete(`/api/v1/spots/${id}`, { headers })
+        .then(response => {
+          return response.data.id
+        })
     },
 
     closeDeleteDialog() {
