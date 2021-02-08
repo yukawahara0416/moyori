b6b8928148c636706da76b3d18384bd4c0673e3b
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-02-08 12:31:54 +0900

    add: テストデータを準備

--- frontend/tests/unit/store/modules/user/user.spec.js
+++ frontend/tests/unit/store/modules/user/user.spec.js
@@ -94,12 +94,10 @@ describe('actions', () => {
   })
 
   it('spotlight', () => {
-    const user = {
-      data: { id: 1 },
-      posts: [{ data: { id: 1, place_id: '123', on: false } }]
-    }
-    const spot = user.posts[0]
-    const tab = 'posts'
+    const place_id = '1234567890test'
+    const tab = 'likes'
+    const spot = { data: { place_id, on: false } }
+    const user = { likes: [spot] }
 
     store.replaceState({ user: user })
     store.dispatch('spotlight', { spot, tab }).then(() => {
