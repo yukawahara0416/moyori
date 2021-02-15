b95f32b3ea6260030120ae3d42274df3dcfcbb34
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-02-15 12:42:40 +0900

    improve: props/spotを詳細に定義

--- frontend/tests/unit/components/Card/CardFrameContent.spec.js
+++ frontend/tests/unit/components/Card/CardFrameContent.spec.js
@@ -31,6 +31,7 @@ describe('props', () => {
   it('spot', () => {
     expect(wrapper.vm.$props.spot).toStrictEqual(propsData.spot)
     expect(wrapper.vm.$props.spot instanceof Spot).toBeTruthy()
+    expect(wrapper.vm.$options.props.spot.required).toBeTruthy()
   })
 })
 
