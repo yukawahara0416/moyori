67017eae860b82e6a14efbc2c6d7e6cbd38701e4
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-02-21 12:08:44 +0900

    improve: テスト性向上のため記法を変更

--- frontend/src/components/Header/HeaderDrawerPages.vue
+++ frontend/src/components/Header/HeaderDrawerPages.vue
@@ -2,10 +2,10 @@
   <v-list-item-group class="staticPages" color="primary">
     <v-list-item :key="id" :to="page.to" v-for="(page, id) in pages">
       <v-list-item-icon>
-        <v-icon v-text="page.icon" />
+        <v-icon>{{ page.icon }}</v-icon>
       </v-list-item-icon>
       <v-list-item-content>
-        <v-list-item-title v-text="page.title" />
+        <v-list-item-title>{{ page.title }}</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
   </v-list-item-group>
