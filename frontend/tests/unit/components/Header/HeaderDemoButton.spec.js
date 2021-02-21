22e65ac8975930c4ddf45280dd0ba496077d82ed
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-02-21 11:30:41 +0900

    improve: shallowMountを追加

--- frontend/tests/unit/components/Header/HeaderDemoButton.spec.js
+++ frontend/tests/unit/components/Header/HeaderDemoButton.spec.js
@@ -1,18 +1,29 @@
-import { mount, createLocalVue } from '@vue/test-utils'
+import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import Component from '@/components/Header/HeaderDemoButton.vue'
 
 const localVue = createLocalVue()
 
 let wrapper
 
-const demoSearch = jest.fn()
-
 beforeEach(() => {
-  wrapper = mount(Component, {
-    localVue,
-    methods: {
-      demoSearch
-    }
+  wrapper = shallowMount(Component, {
+    localVue
+  })
+})
+
+describe('v-on', () => {
+  it('demoSearch', () => {
+    const demoSearch = jest.fn()
+
+    wrapper = mount(Component, {
+      localVue,
+      methods: {
+        demoSearch
+      }
+    })
+
+    wrapper.find('.v-btn').trigger('click')
+    expect(demoSearch).toHaveBeenCalled()
   })
 })
 
