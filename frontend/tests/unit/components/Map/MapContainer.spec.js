2648f968501ca7645547f4d16024675029757da1
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-02-23 06:42:48 +0900

    improve: specにexpectを追加

--- frontend/tests/unit/components/Map/MapContainer.spec.js
+++ frontend/tests/unit/components/Map/MapContainer.spec.js
@@ -214,7 +214,18 @@ describe('computed', () => {
 describe('methods', () => {
   it('beforeSearch', () => {
     wrapper.vm.beforeSearch()
-    expect(dialog.actions.dialogOff).toHaveBeenCalledTimes(3)
+    expect(dialog.actions.dialogOff).toHaveBeenCalledWith(
+      expect.any(Object),
+      'dialogSign'
+    )
+    expect(dialog.actions.dialogOff).toHaveBeenCalledWith(
+      expect.any(Object),
+      'dialogSpotCreate'
+    )
+    expect(dialog.actions.dialogOff).toHaveBeenCalledWith(
+      expect.any(Object),
+      'dialogSpotEdit'
+    )
     expect(loading.mutations.loadingOn).toHaveBeenCalled()
     expect(spot.mutations.clearSpots).toHaveBeenCalled()
   })
