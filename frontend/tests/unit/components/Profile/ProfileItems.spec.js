90e2ad29c6b1b510207fad1c6dff15efe5609de0
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-02-27 05:43:30 +0900

    add: spec追加 v-if

--- frontend/tests/unit/components/Profile/ProfileItems.spec.js
+++ frontend/tests/unit/components/Profile/ProfileItems.spec.js
@@ -40,6 +40,18 @@ describe('template', () => {
     )
   })
 
+  it('v-if true', async () => {
+    await wrapper.setProps({ user: { data: { id: 1 } } })
+    expect(wrapper.find(ProfileItemsImage).exists()).toBeTruthy()
+    expect(wrapper.find(ProfileItemsName).exists()).toBeTruthy()
+  })
+
+  it('v-if false', async () => {
+    await wrapper.setProps({ user: {} })
+    expect(wrapper.find(ProfileItemsImage).exists()).toBeFalsy()
+    expect(wrapper.find(ProfileItemsName).exists()).toBeFalsy()
+  })
+
   it('snapshot', () => {
     expect(wrapper.vm.$el).toMatchSnapshot()
   })
