d655aa586f634a77a3202e4488b8ce11876fe102
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-02-27 19:11:08 +0900

    add: vee-validateをimport

--- frontend/tests/unit/components/Sign/SignTabitemsSignupEmail.spec.js
+++ frontend/tests/unit/components/Sign/SignTabitemsSignupEmail.spec.js
@@ -1,9 +1,25 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
+import { ValidationObserver, ValidationProvider, extend } from 'vee-validate'
 import Component from '@/components/Sign/SignTabitemsSignupEmail.vue'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
+localVue.component('ValidationObserver', ValidationObserver)
+localVue.component('ValidationProvider', ValidationProvider)
+
+const {
+  required,
+  email,
+  max,
+  min,
+  alpha_num
+} = require('vee-validate/dist/rules.umd')
+extend('required', required)
+extend('email', email)
+extend('max', max)
+extend('min', min)
+extend('alpha_num', alpha_num)
 
 let wrapper
 let store
