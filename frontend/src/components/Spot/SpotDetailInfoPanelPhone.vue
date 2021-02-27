8812e9cfd2da1a1c164de1baf25bc7d3bea9528b
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-02-28 08:31:50 +0900

    improve: props/spotを詳細に定義

--- frontend/src/components/Spot/SpotDetailInfoPanelPhone.vue
+++ frontend/src/components/Spot/SpotDetailInfoPanelPhone.vue
@@ -8,9 +8,17 @@
 </template>
 
 <script>
+import { Spot } from '@/class/Spot.js'
+
 export default {
   props: {
-    spot: Object
+    spot: {
+      type: Object,
+      default: () => {
+        return new Spot()
+      },
+      required: true
+    }
   },
 
   computed: {
