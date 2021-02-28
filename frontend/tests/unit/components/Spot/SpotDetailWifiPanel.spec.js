37993c77dc0b9399815ffdccc0ae7b5333b44144
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-02-28 09:52:29 +0900

    improve: props/spotを詳細に定義

--- frontend/tests/unit/components/Spot/SpotDetailWifiPanel.spec.js
+++ frontend/tests/unit/components/Spot/SpotDetailWifiPanel.spec.js
@@ -1,4 +1,5 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
+import { Spot } from '@/class/Spot.js'
 import Component from '@/components/Spot/SpotDetailWifiPanel.vue'
 
 const localVue = createLocalVue()
@@ -8,11 +9,11 @@ let propsData
 
 beforeEach(() => {
   propsData = {
-    spot: {
+    spot: new Spot({
       data: { id: 1 },
       wifi_withs: [{ id: 2 }, { id: 3 }],
       wifi_withouts: [{ id: 4 }, { id: 5 }]
-    }
+    })
   }
 
   wrapper = shallowMount(Component, {
@@ -24,7 +25,8 @@ beforeEach(() => {
 describe('props', () => {
   it('spot', () => {
     expect(wrapper.vm.$props.spot).toStrictEqual(propsData.spot)
-    expect(wrapper.vm.$props.spot instanceof Object).toBeTruthy()
+    expect(wrapper.vm.$props.spot instanceof Spot).toBeTruthy()
+    expect(wrapper.vm.$options.props.spot.required).toBeTruthy()
   })
 })
 
