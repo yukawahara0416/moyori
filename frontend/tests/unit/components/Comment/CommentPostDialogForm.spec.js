ad5376f668d8a177e7a760382e5d2ecbc42264c9
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-03-01 20:55:31 +0900

    improve: expectを追加

--- frontend/tests/unit/components/Comment/CommentPostDialogForm.spec.js
+++ frontend/tests/unit/components/Comment/CommentPostDialogForm.spec.js
@@ -934,17 +934,34 @@ describe('methods', () => {
 })
 
 describe('template', () => {
-  it('v-btn hasClass small', () => {
-    Object.assign(window, { innerWidth: 959 })
+  it('v-btn has small', () => {
+    const smAndDown = wrapper.vm.$vuetify.breakpoint.thresholds.sm - 1
+    Object.assign(window, { innerWidth: smAndDown })
 
     wrapper = mount(Component, {
       localVue,
       propsData,
-      vuetify,
-      stubs: []
+      vuetify
     })
 
+    expect(wrapper.vm.$vuetify.breakpoint.smAndDown).toBeTruthy()
     expect(wrapper.find('.v-btn').classes()).toContain('v-size--small')
+
+    Object.assign(window, { innerWidth: 1024 })
+  })
+
+  it('v-btn not has small', () => {
+    const md = wrapper.vm.$vuetify.breakpoint.thresholds.md
+    Object.assign(window, { innerWidth: md })
+
+    wrapper = mount(Component, {
+      localVue,
+      propsData,
+      vuetify
+    })
+
+    expect(wrapper.vm.$vuetify.breakpoint.smAndDown).toBeFalsy()
+    expect(wrapper.find('.v-btn').classes()).toContain('v-size--default')
 
     Object.assign(window, { innerWidth: 1024 })
   })
