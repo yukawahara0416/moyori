35a31d4c369de885ec07d53aba8e4ef08cd0da1b
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-03-05 08:52:59 +0900

    add: テスト候補を書き出し

---
+++ frontend/tests/unit/components/Spot/SpotDeleteButton.spec.js
@@ -0,0 +1,27 @@
+import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
+import { Spot } from '@/class/Spot.js'
+
+beforeEach(() => {})
+
+describe('props', () => {
+  it('spot', () => {})
+})
+
+describe('v-on', () => {
+  it('click openDialog', () => {})
+  it('click closeDeleteDialog', () => {})
+  it('click closeDetailDialog', () => {})
+})
+
+describe('methods', () => {
+  it('openDialog', () => {})
+  it('closeDeleteDialog', () => {})
+})
+
+describe('emit', () => {
+  it('closeDetailDialog', () => {})
+})
+
+describe('template', () => {
+  it('SpotDeleteButtonDialog has :spot', () => {})
+})
