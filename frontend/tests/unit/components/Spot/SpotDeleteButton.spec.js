a2f378b2cd85738fefabc7ba4b9158b84fa75922
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-03-05 12:52:17 +0900

    add: spec追加 props/spot

--- frontend/tests/unit/components/Spot/SpotDeleteButton.spec.js
+++ frontend/tests/unit/components/Spot/SpotDeleteButton.spec.js
@@ -5,9 +5,26 @@ import Component from '@/components/Spot/SpotDeleteButton.vue'
 const localVue = createLocalVue()
 
 let wrapper
+let propsData
+
+beforeEach(() => {
+  propsData = {
+    spot: new Spot({
+      data: { id: 1 }
+    })
+  }
+  wrapper = shallowMount(Component, {
+    localVue,
+    propsData
+  })
+})
 
 describe('props', () => {
-  it('spot', () => {})
+  it('spot', () => {
+    expect(wrapper.vm.$props.spot).toStrictEqual(propsData.spot)
+    expect(wrapper.vm.$props.spot instanceof Spot).toBeTruthy()
+    expect(wrapper.vm.$options.props.spot.required).toBeTruthy()
+  })
 })
 
 describe('v-on', () => {
